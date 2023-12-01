96d8de1fcf44a721c23e141c610a14d7782ad090
Peter van der Heijden <heijp06@gmail.com>
2023-12-01 09:30:37 +0100

    Solve day 1 part 2.

--- 2023/day01/src/Lib.hs
+++ 2023/day01/src/Lib.hs
@@ -1,17 +1,56 @@
+{-# LANGUAGE OverloadedStrings, TupleSections #-}
+
 module Lib
     ( part1
     , part2
     ) where
 
 import Data.Char (isDigit, digitToInt)
+import Data.Function (on)
+import Data.List (minimumBy, maximumBy)
+import Data.Text (Text, pack)
+import Data.Text.Internal.Search (indices)
+
+digitMap :: [(Text, Int)]
+digitMap = [ ("one", 1)
+           , ("1", 1)
+           , ("two", 2)
+           , ("2", 2)
+           , ("three", 3)
+           , ("3", 3)
+           , ("four", 4)
+           , ("4", 4)
+           , ("five", 5)
+           , ("5", 5)
+           , ("six", 6)
+           , ("6", 6)
+           , ("seven", 7)
+           , ("7", 7)
+           , ("eight", 8)
+           , ("8", 8)
+           , ("nine", 9)
+           , ("9", 9)
+           ]
 
 part1 :: [String] -> Int
 part1 = foldr add 0
 
 part2 :: [String] -> Int
-part2 = undefined
+part2 = foldr count 0
+
+count :: String -> Int -> Int
+count xs acc = acc + (10 * left) + right
+    where
+        left = snd . minimumBy (compare `on` fst) $ digitsWithIndices xs
+        right = snd . maximumBy (compare `on` fst) $ digitsWithIndices xs
 
 add :: String -> Int -> Int
 add xs acc = acc + (10 * head digits) + last digits
     where
         digits = [ digitToInt c | c <- xs, isDigit c ]
+
+digitsWithIndices :: String -> [(Int, Int)]
+digitsWithIndices xs = foldr (addIndices $ pack xs) [] digitMap
+
+addIndices :: Text -> (Text, Int) -> [(Int, Int)] -> [(Int, Int)]
+addIndices line (name, value) acc = map (, value) (indices name line) ++ acc
--- 2023/day01/test/Spec.hs
+++ 2023/day01/test/Spec.hs
@@ -2,8 +2,27 @@ import Test.Hspec hiding (example)
 
 import Lib
 
+example1 :: [String]
+example1 = [ "1abc2"
+           , "pqr3stu8vwx"
+           , "a1b2c3d4e5f"
+           , "treb7uchet"
+           ]
+
+example2 :: [String]
+example2 = [ "two1nine"
+           , "eightwothree"
+           , "abcone2threexyz"
+           , "xtwone3four"
+           , "4nineeightseven2"
+           , "zoneight234"
+           , "7pqrstsixteen"
+           ]
+
 main :: IO ()
 main = hspec $ do
     describe "Part 1" $ do
-        it "part1" $ 1 `shouldBe` 1
-
+        it "part1 example1 -> 142" $ part1 example1 `shouldBe` 142
+
+    describe "Part 2" $ do
+        it "part2 example2 -> 281" $ part2 example2 `shouldBe` 281
