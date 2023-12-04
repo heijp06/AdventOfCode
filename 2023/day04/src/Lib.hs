b952333cde985bf04317da93d8cc52acf8a2276f
Peter van der Heijden <peter.vd.heijden@malvernpanalytical.com>
2023-12-04 16:58:57 +0100

    Refactor day 4 part 1.

--- 2023/day04/src/Lib.hs
+++ 2023/day04/src/Lib.hs
@@ -14,13 +14,15 @@ part2 :: [String] -> Int
 part2 = undefined
 
 score :: String -> Int
-score xs = if count == 0 then 0 else 2 ^ (count - 1)
+score xs = let c = count xs in if c == 0 then 0 else 2 ^ (c - 1)
+
+count :: String -> Int
+count xs = Set.size $ Set.intersection winners actual
     where
         (_, values) = splitInTwo ": " xs
         (left, right) = splitInTwo " | " values
         winners = Set.fromList . filter (not . null) $ splitOn " " left
         actual = Set.fromList . filter (not . null) $ splitOn " " right
-        count = Set.size $ Set.intersection winners actual
 
 splitInTwo :: String -> String -> (String, String)
 splitInTwo delimiter xs = case splitOn delimiter xs of
