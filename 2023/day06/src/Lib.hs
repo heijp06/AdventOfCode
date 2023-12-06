074c800c3b9e3632ce7aea29df14bf29d2630ac2
Peter van der Heijden <heijp06@gmail.com>
2023-12-06 06:59:21 +0100

    Solve day 6 part 2.

--- 2023/day06/src/Lib.hs
+++ 2023/day06/src/Lib.hs
@@ -6,19 +6,23 @@ module Lib
     , part2
     ) where
 
+import Data.Char (isDigit)
 import Data.List.Split (splitOn)
 
 part1 :: [String] -> Int
 part1 = product . map (uncurry count) . parse
 
 part2 :: [String] -> Int
-part2 = undefined
+part2 xs = count (largeNumber $ head xs) (largeNumber $ xs !! 1)
 
 parse :: [String] -> [(Int, Int)]
 parse xs = zip (numbers $ head xs) $ numbers (xs !! 1)
 
 numbers :: String -> [Int]
 numbers xs = [ read x | x <- tail $ splitOn " " xs, not $ null x ]
+
+largeNumber :: String -> Int
+largeNumber = read . filter isDigit
 
 count :: Int -> Int -> Int
 count t d = t1 - t0 + 1 - delta
