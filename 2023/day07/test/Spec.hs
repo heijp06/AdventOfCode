43cbec745f238b6a83eab1d360d96e9636f65f78
Peter van der Heijden <heijp06@gmail.com>
2023-12-07 08:32:00 +0100

    Start day 7 part 2.

--- 2023/day07/test/Spec.hs
+++ 2023/day07/test/Spec.hs
@@ -13,8 +13,11 @@ example = [ "32T3K 765"
 main :: IO ()
 main = hspec $ do
     describe "parse" $ do
-        it "parse" $ parse (head example) `shouldBe` (OnePair [ Three, Two, Ten, Three, King ], 765)
+        it "parse" $ parse (const False) (head example) `shouldBe` (OnePair [ Three, Two, Ten, Three, King ], 765)
 
     describe "Part 1" $ do
         it "part1 example" $ part1 example `shouldBe` 6440
 
+    describe "Part 2" $ do
+        it "part2 example" $ part2 example `shouldBe` 5905
+        it "part2 joker < one" $ part2 [ "JQQQQ 1", "1QQQQ 10" ] `shouldBe` 21
