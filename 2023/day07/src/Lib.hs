4caa09272e9e2384f22b3fd232a6989eccfc4ae1
Peter van der Heijden <peter.vd.heijden@malvernpanalytical.com>
2023-12-07 09:35:41 +0100

    Solve day 7 part 2.

--- 2023/day07/src/Lib.hs
+++ 2023/day07/src/Lib.hs
@@ -11,22 +11,6 @@ import Data.List (group, sort)
 data Card = Joker | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace
                 deriving (Eq, Ord, Show)
 
-instance Read Card where
-    readsPrec _ "A" = [(Ace, "")]
-    readsPrec _ "K" = [(King, "")]
-    readsPrec _ "Q" = [(Queen, "")]
-    readsPrec _ "J" = [(Jack, "")]
-    readsPrec _ "T" = [(Ten, "")]
-    readsPrec _ "9" = [(Nine, "")]
-    readsPrec _ "8" = [(Eight, "")]
-    readsPrec _ "7" = [(Seven, "")]
-    readsPrec _ "6" = [(Six, "")]
-    readsPrec _ "5" = [(Five, "")]
-    readsPrec _ "4" = [(Four, "")]
-    readsPrec _ "3" = [(Three, "")]
-    readsPrec _ "2" = [(Two, "")]
-    readsPrec _ _ = []
-
 data Ord a => Hand a = HighCard a
                      | OnePair a
                      | TwoPair a
@@ -36,31 +20,51 @@ data Ord a => Hand a = HighCard a
                      | FiveOfAKind a deriving (Eq, Ord, Show)
 
 part1 :: [String] -> Int
-part1 = sum . zipWith (*) [1..] . map snd . sort . map (parse (const False))
+part1 = solve id
 
 part2 :: [String] -> Int
-part2 = sum . zipWith (*) [1..] . map snd . sort . map (parse (==Jack))
+part2 = solve replaceJackByJoker
 
-parse :: (Card -> Bool) -> String -> (Hand [Card], Int)
-parse f xs = case words xs of
-                [hand, bid] -> (parseHand f hand, read bid)
-                _ -> error $ "Cannot parse: " ++ xs
+solve :: (Card -> Card) -> [String] -> Int
+solve replace = sum . zipWith (*) [1..] . map snd . sort . map (parse replace)
 
-parseHand :: (Card -> Bool) -> String -> Hand [Card]
-parseHand f xs = case replaced of
-                [5] -> FiveOfAKind cards
-                [1, 4] -> FourOfAKind cards
-                [2, 3] -> FullHouse cards
-                [1, 1, 3] -> ThreeOfAKind cards
-                [1, 2, 2] -> TwoPair cards
-                [1, 1, 1, 2] -> OnePair cards
-                [1, 1, 1, 1, 1] -> HighCard cards
-                _ -> error $ "Cannot parse hand: " ++ xs
+replaceJackByJoker :: Card -> Card
+replaceJackByJoker Jack = Joker
+replaceJackByJoker c = c
+
+parse :: (Card -> Card) -> String -> (Hand [Card], Int)
+parse replace xs = case words xs of
+                    [hand, bid] -> (parseHand replace hand, read bid)
+                    _ -> error $ "Cannot parse: " ++ xs
+
+parseHand :: (Card -> Card) -> String -> Hand [Card]
+parseHand replace xs = case typeOfHand of
+                        [5] -> FiveOfAKind cards
+                        [1, 4] -> FourOfAKind cards
+                        [2, 3] -> FullHouse cards
+                        [1, 1, 3] -> ThreeOfAKind cards
+                        [1, 2, 2] -> TwoPair cards
+                        [1, 1, 1, 2] -> OnePair cards
+                        [1, 1, 1, 1, 1] -> HighCard cards
+                        _ -> error $ "Cannot parse hand: " ++ xs
     where
-        cards = map (read . (:[])) xs :: [Card]
-        typeOfHand = sort . map length . group . sort $ filter (not . f) cards
-        jokers = length $ filter f cards
-        replaced
-            | jokers == 0 = typeOfHand
-            | jokers == 5 = [5]
-            | otherwise = init typeOfHand ++ [last typeOfHand + jokers]
+        cards = map (replace . parseCard) xs
+        typeOfHandWithoutJokers = sort . map length . group . sort $ filter (/=Joker) cards
+        jokers = length $ filter (==Joker) cards
+        typeOfHand = if jokers == 5 then [5] else init typeOfHandWithoutJokers ++ [last typeOfHandWithoutJokers + jokers]
+
+parseCard :: Char -> Card
+parseCard 'A' = Ace
+parseCard 'K' = King
+parseCard 'Q' = Queen
+parseCard 'J' = Jack
+parseCard 'T' = Ten
+parseCard '9' = Nine
+parseCard '8' = Eight
+parseCard '7' = Seven
+parseCard '6' = Six
+parseCard '5' = Five
+parseCard '4' = Four
+parseCard '3' = Three
+parseCard '2' = Two
+parseCard c = error $ "Cannot parse card: " ++ [c]
--- 2023/day07/test/Spec.hs
+++ 2023/day07/test/Spec.hs
@@ -13,12 +13,12 @@ example = [ "32T3K 765"
 main :: IO ()
 main = hspec $ do
     describe "parse" $ do
-        it "parse" $ parse (const False) (head example) `shouldBe` (OnePair [ Three, Two, Ten, Three, King ], 765)
-        it "parse" $ parse (const False) (head example) `shouldBe` (OnePair [ Three, Two, Ten, Three, King ], 765)
+        it "parse" $ parse id (head example) `shouldBe` (OnePair [ Three, Two, Ten, Three, King ], 765)
 
     describe "Part 1" $ do
         it "part1 example" $ part1 example `shouldBe` 6440
+        it "part1 jack > three" $ part1 [ "J2222 1", "32222 10" ] `shouldBe` 12
 
     describe "Part 2" $ do
         it "part2 example" $ part2 example `shouldBe` 5905
-        it "part2 joker < one" $ part2 [ "JQQQQ 1", "1QQQQ 10" ] `shouldBe` 21
+        it "part2 joker < two" $ part2 [ "J2222 1", "22222 10" ] `shouldBe` 21
