e147f7d15621a783d124cfa492d1eccf54386649
Peter van der Heijden <heijp06@gmail.com>
2023-12-08 08:17:43 +0100

    Solve day 8 part 1.

--- 2023/day08/src/Lib.hs
+++ 2023/day08/src/Lib.hs
@@ -10,15 +10,27 @@ import qualified Data.Map as Map
 type Network = Map.Map String (String, String)
 
 part1 :: [String] -> Int
-part1 xs = fst . foldl (step network) (0, "AAA") $ cycle directions
+part1 xs = step network 0 "AAA" $ cycle directions
     where
         (directions, network) = parse xs
 
 part2 :: [String] -> Int
 part2 = undefined
 
-step :: Network -> (Int, String) -> Char -> (Int, String)
-step = undefined
+step :: Network -> Int -> String -> String -> Int
+step _ _ _ [] = error "List of directions should be infinitely long."
+step network steps current (x:xs) = if next == "ZZZ"
+                                        then steps + 1
+                                        else step network (steps + 1) next xs
+    where
+        (left, right) = network ! current
+        next = case x of
+                'L' -> left
+                'R' -> right
+                _ -> error $ "Invalid direction: " ++ [x]
+
+-- step :: Network -> (Int, String) -> Char -> (Int, String)
+-- step network (steps, current) dir = if next == "ZZZ" then (steps + 1, "ZZZ") else (steps + 1, next)
 
 parse :: [String] -> (String, Network)
 parse xs = (directions, network)
