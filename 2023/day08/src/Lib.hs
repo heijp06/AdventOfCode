99295e725eefa7856a2fe9e3112f730266004f13
Peter van der Heijden <peter.vd.heijden@malvernpanalytical.com>
2023-12-08 13:58:58 +0100

    Refactor day 8.

--- 2023/day08/src/Lib.hs
+++ 2023/day08/src/Lib.hs
@@ -10,35 +10,21 @@ import qualified Data.Map as Map
 type Network = Map.Map String (String, String)
 
 part1 :: [String] -> Int
-part1 xs = step network 0 "AAA" $ cycle directions
+part1 = solve "AAA"
+
+part2 :: [String] -> Int
+part2 xs = foldr1 lcm [ solve start xs | start <- Map.keys network, start !! 2 == 'A' ]
+    where
+        (_, network) = parse xs
+
+solve :: String -> [String] -> Int
+solve start xs = step network 0 start $ cycle directions
     where
         (directions, network) = parse xs
 
-part2 :: Int
-part2 = foldr1 lcm [13939, 11309, 20777, 15517, 17621, 18673]
--- part2 :: [String] -> Int
--- part2 xs = error . show
---                  . take 10
---                  . map fst
---                  . filter ((=='Z') . (!!2) . snd)
---                  . zip [0..]
---                  . map snd $ iterate (go network) (cycle directions, "MNA")
---     where
---         (directions, network) = parse xs
-
--- go :: Network -> (String, String) -> (String, String)
--- go _ ([], _) = error "List of directions should be infinitely long."
--- go network (x : xs, current) = (xs, next)
---     where
---         (left, right) = network ! current
---         next = case x of
---                 'L' -> left
---                 'R' -> right
---                 _ -> error $ "Invalid direction: " ++ [x]
-
 step :: Network -> Int -> String -> String -> Int
 step _ _ _ [] = error "List of directions should be infinitely long."
-step network steps current (x:xs) = if next == "ZZZ"
+step network steps current (x:xs) = if next !! 2 == 'Z'
                                         then steps + 1
                                         else step network (steps + 1) next xs
     where
@@ -52,4 +38,4 @@ parse :: [String] -> (String, Network)
 parse xs = (directions, network)
     where
         directions = head xs
-        network = Map.fromList [ (take 3 x, ((take 3 . drop 7) x, (take 3 . drop 12) x)) | x <- (tail . tail) xs ]
+        network = Map.fromList [ (take 3 x, (take 3 $ drop 7 x, take 3 $ drop 12 x)) | x <- tail $ tail xs ]
