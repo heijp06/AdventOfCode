f919899ace0d31a525cfed3fab05b195248fda9e
Peter van der Heijden <heijp06@gmail.com>
2023-12-10 06:50:20 +0100

    Start day 10 part 1.

---
+++ 2023/day10/src/Lib.hs
@@ -0,0 +1,15 @@
+module Lib
+    ( part1
+    , part2
+    ) where
+
+type Position = (Int, Int)
+
+part1 :: [String] -> Int
+part1 = undefined
+
+part2 :: [String] -> Int
+part2 = undefined
+
+add :: Position -> Position -> Position
+add (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
