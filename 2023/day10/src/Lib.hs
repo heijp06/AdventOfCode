03063ee4b82501279e5bdbb732d6808241cd5413
Peter van der Heijden <heijp06@gmail.com>
2023-12-10 06:57:23 +0100

    Solve day 10 part 1.

--- 2023/day10/src/Lib.hs
+++ 2023/day10/src/Lib.hs
@@ -1,15 +1,84 @@
 module Lib
-    ( part1
+    ( next
+    , parse
+    , part1
     , part2
+    , reachable
+    , directionsAfterStart
     ) where
 
+import qualified Data.Map as Map
+
+-- import Debug.Trace (trace)
+-- import Text.Printf (printf)
+
 type Position = (Int, Int)
+type Pipe = (Position, Position)
+type Grid = Map.Map Position Pipe
 
 part1 :: [String] -> Int
-part1 = undefined
+part1 xs = ((+1) . length . takeWhile ((/=start) . fst) . drop 1 $ iterate (next grid) (start, add start dir)) `div` 2
+    where
+        (grid, start) = parse xs
+        dir = head $ directionsAfterStart grid start
 
 part2 :: [String] -> Int
 part2 = undefined
 
+directionsAfterStart :: Grid -> Position -> [Position]
+directionsAfterStart grid start = [ direction | direction <- [(0, -1), (1, 0), (0, 1), (-1, 0)]
+                                              , reachable grid start direction ]
+
+reachable :: Grid -> Position -> Position -> Bool
+reachable grid current@(x, y) dir@(dx, dy) = result
+    where
+        target = (x + dx, y + dy)
+        (dirCurrent1, dirCurrent2) = Map.findWithDefault noPipe current grid
+        (dirTarget1, dirTarget2) = Map.findWithDefault noPipe target grid
+        result = dir `elem` [dirCurrent1, dirCurrent2] && (dir == neg dirTarget1 || dir == neg dirTarget2)
+
+noPipe :: Pipe
+noPipe = ((0, 0), (0, 0))
+
+next :: Grid -> (Position, Position) -> (Position, Position)
+next grid (current, previous) = head [ (add current d, current) | d <- [(0, -1), (1, 0), (0, 1), (-1, 0)]
+                                                                , reachable grid current d
+                                                                , add current d /= previous
+                                                                ]
+
 add :: Position -> Position -> Position
 add (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+neg :: Position -> Position
+neg (x, y) = (-x, -y)
+
+parse :: [String] -> (Grid, Position)
+parse xs = case dirs of
+            [dir1, dir2] -> (Map.insert start (dir1, dir2) grid, start)
+            _ -> error $ "Cannot build pipe for start: " ++ show dirs
+    where
+        (grid, start) = parse' xs
+        dirs = [ d
+               | d <- [(0, 1), (1, 0), (0, -1), (-1, 0)]
+               , let (d1, d2) = Map.findWithDefault noPipe (add start d) grid in d1 == neg d || d2 == neg d
+               ]
+
+parse' :: [String] -> (Grid, Position)
+parse' xs = foldr addTile (Map.empty, (-1, -1)) $ zip [ (x, y) | y <- [0..height-1], x <- [0..width-1]] $ concat xs
+    where
+        height = length xs
+        width = length $ head xs
+
+addTile :: (Position, Char) -> (Grid, Position) -> (Grid, Position)
+addTile (pos, tile) (grid, start) = case tile of
+                                        '|' -> insert ((0, -1), (0, 1))
+                                        '-' -> insert ((-1, 0), (1, 0))
+                                        'L' -> insert ((0, -1), (1, 0))
+                                        'J' -> insert ((-1, 0), (0, -1))
+                                        '7' -> insert ((-1, 0), (0, 1))
+                                        'F' -> insert ((0, 1), (1, 0))
+                                        '.' -> (grid, start)
+                                        'S' -> (grid, pos)
+                                        _ -> error $ "Unexpected tile: " ++ show tile
+    where
+        insert pipe = (Map.insert pos pipe grid, start)
--- 2023/day10/test/Spec.hs
+++ 2023/day10/test/Spec.hs
@@ -1,9 +1,67 @@
 import Test.Hspec hiding (example)
+import Test.Hspec.Runner (configFailFast, defaultConfig, hspecWith)
+
+import qualified Data.Map as Map
 
 import Lib
 
 main :: IO ()
-main = hspec $ do
+main = hspecWith defaultConfig {configFailFast = True} $ do
+    describe "parse" $ do
+        it "parse" $ parse exampleParse `shouldBe` ( Map.fromList [ ((0, 0), ((0, -1), (0, 1)))
+                                                                  , ((1, 0), ((-1, 0), (1, 0)))
+                                                                  , ((2, 0), ((0, 1), (1, 0)))
+                                                                  , ((3, 0), ((-1, 0), (0, 1)))
+                                                                  , ((0, 1), ((-1, 0), (0, -1)))
+                                                                  , ((2, 1), ((0, -1), (1, 0)))
+                                                                  , ((3, 1), ((0, -1), (-1, 0)))
+                                                                  ], (3, 1) )
+
+    describe "reachable" $ do
+        it "reachable example1 N" $ uncurry reachable (parse example1) (0, -1) `shouldBe` False
+        it "reachable example1 E" $ uncurry reachable (parse example1) (1, 0) `shouldBe` True
+        it "reachable example1 S" $ uncurry reachable (parse example1) (0, 1) `shouldBe` True
+        it "reachable example1 W" $ uncurry reachable (parse example1) (-1, 0) `shouldBe` False
+        it "reachable example1 (1, 2) (1, 0)" $ let (grid, _) = parse example1
+                                                in reachable grid (1, 2) (1, 0) `shouldBe` False
+
+    describe "directionsAfterStart" $ do
+        it "directionsAfterStart example1" $ uncurry directionsAfterStart (parse example1) `shouldBe` [ (1, 0), (0, 1) ]
+        it "directionsAfterStart example2" $ uncurry directionsAfterStart (parse example2) `shouldBe` [ (1, 0), (0, 1) ]
+    
+    describe "next" $ do
+        it "next example1 start" $ let (grid, start) = parse example1
+                                   in next grid (start, (2, 1)) `shouldBe` ((1, 2), start)
+        it "next example1 start" $ let (grid, start) = parse example1
+                                   in next grid ((1, 2), start) `shouldBe` ((1, 3), (1, 2))
+
     describe "Part 1" $ do
-        it "part1" $ 1 `shouldBe` 1
-
+        it "part1 example1" $ part1 example1 `shouldBe` result1
+        it "part1 example2" $ part1 example2 `shouldBe` result2
+
+exampleParse :: [String]
+exampleParse = [ "|-F7"
+               , "J.LS"
+               ]
+
+example1 :: [String]
+example1 = [ "-L|F7"
+           , "7S-7|"
+           , "L|7||"
+           , "-L-J|"
+           , "L|-JF"
+           ]
+
+result1 :: Int
+result1 = 4
+
+example2 :: [String]
+example2 = [ "7-F7-"
+           , ".FJ|7"
+           , "SJLL7"
+           , "|F--J"
+           , "LJ.LJ"
+           ]
+
+result2 :: Int
+result2 = 8
