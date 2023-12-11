32a62424960ac96061a5a37ea0ce641796726563
Peter van der Heijden <heijp06@gmail.com>
2023-12-11 17:57:42 +0100

    Continue day 11 part 1.

--- 2023/day11/src/Lib.hs
+++ 2023/day11/src/Lib.hs
@@ -5,27 +5,44 @@ module Lib
     , part2
     ) where
 
+import Data.List (sort, tails)
 import Data.Tuple (swap)
+
+-- import Debug.Trace (trace)
+-- import Text.Printf (printf)
 
 type Position = (Int, Int)
 
 part1 :: [String] -> Int
-part1 = undefined
+part1 xs = foldr add 0 gss
+    where
+        positions = expand $ parse xs
+        gss = init $ tails positions
 
 part2 :: [String] -> Int
 part2 = undefined
 
+add :: [Position] -> Int -> Int
+add [] _ = error "Empty list of positions"
+add (pos:ps) acc = acc + foldr (manhattan pos) 0 ps
+
+manhattan :: Position -> Position -> Int -> Int
+manhattan (x1, y1) (x2, y2) acc = acc + abs (x2 - x1) + abs (y2 - y1)
+
 parse :: [String] -> [Position]
-parse xs = [ pos | (pos, c) <- (zip ((,) <$> [0..height-1] <*> [0..width-1]) $ concat xs), c == '#' ]
+parse xs = [ pos | (pos, c) <- zip ((,) <$> [0..height-1] <*> [0..width-1]) $ concat xs, c == '#' ]
     where
         height = length xs
         width = length $ head xs
 
 expand :: [Position] -> [Position]
-expand positions = map swap expanded
+expand positions = sort $ map swap expanded
     where
         (_, _, expandedHeight) = foldl expand' (0, 0, []) positions
-        (_, _, expanded) = foldl expand' (0, 0, []) $ map swap expandedHeight
+        (_, _, expanded) = foldl expand' (0, 0, []) . sort $ map swap expandedHeight
 
-expand' :: (Int, Int, [Position]) -> Position -> (Int, Int, Position)
-expand' = undefined
+expand' :: (Int, Int, [Position]) -> Position -> (Int, Int, [Position])
+expand' (_, _, []) pos@(x, _) = (0, x, [pos])
+expand' (extra, x', ps) (x, y) = (newExtra, x, ps ++ [(x + newExtra, y)])
+    where
+        newExtra = extra + max 0 (x - x' - 1)
--- 2023/day11/test/Spec.hs
+++ 2023/day11/test/Spec.hs
@@ -1,4 +1,5 @@
 import Test.Hspec hiding (example)
+import Test.Hspec.Runner (configFailFast, defaultConfig, hspecWith)
 
 import Lib
 
@@ -16,12 +17,14 @@ example = [ "...#......"
           ]
 
 main :: IO ()
-main = hspec $ do
+main = hspecWith defaultConfig {configFailFast = True} $ do
     describe "parse" $ do
-        it "parse example" $ parse example `shouldBe` [(0, 3), (1, 7), (2, 0), (4, 6), (5, 1), (6, 9), (8, 7), (9, 0), (9, 4)]
+        it "parse example" $ parse example `shouldBe`
+            [(0, 3), (1, 7), (2, 0), (4, 6), (5, 1), (6, 9), (8, 7), (9, 0), (9, 4)]
     
     describe "expand" $ do
-        it "expand example" $ expand example `shouldBe` [(0, 4), (1, 9), (2, 0), (5, 8), (6, 1), (7, 12), (10, 9), (11, 0), (11, 5)]
+        it "expand example" $ expand (parse example) `shouldBe`
+            [(0, 4), (1, 9), (2, 0), (5, 8), (6, 1), (7, 12), (10, 9), (11, 0), (11, 5)]
 
     describe "Part 1" $ do
         it "part1 example" $ part1 example `shouldBe` 374
