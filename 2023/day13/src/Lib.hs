1e7699831014f0a40ce1884ffd0b8661082cb153
Peter van der Heijden <peter.vd.heijden@malvernpanalytical.com>
2023-12-13 11:18:52 +0100

    Trying to solve day 13 part 2.

--- 2023/day13/src/Lib.hs
+++ 2023/day13/src/Lib.hs
@@ -6,44 +6,57 @@ module Lib
     , reflectHorizontal
     , reflectVertical
     , replace
+    , smudge
     ) where
 
 import Data.List (inits, tails, transpose)
+import Text.Printf (printf)
+
+import Debug.Trace (trace)
 
 type Position = (Int, Int)
+type Valley = [String]
 
 part1 :: [String] -> Int
 part1 = sum . map reflect . parse
 
 part2 :: [String] -> Int
-part2 = undefined
+part2 = sum . map smudge . parse
 
-reflect :: [String] -> Int
+reflect :: Valley -> Int
 reflect xs = 100 * reflectHorizontal xs + reflectVertical xs
 
-reflectHorizontal :: [String] -> Int
+reflectHorizontal :: Valley -> Int
 reflectHorizontal xs = case (top, bottom) of
                         ([], []) -> 0
                         (_, []) -> length (head top) `div` 2
                         ([], _) -> length xs - length (head bottom) `div` 2
-                        _ -> error "Reflection from both top and bottom"
+                        _ -> error $ printf "Reflection from both top and bottom, top = %s, bottom = %s." (show top) (show bottom)
     where
         top = [ s | s <- inits xs, not $ null s, even $ length s, s == reverse s ]
         bottom = [ s | s <- tails xs, not $ null s, even $ length s, s == reverse s ]
 
-reflectVertical :: [String] -> Int
+reflectVertical :: Valley -> Int
 reflectVertical = reflectHorizontal . transpose
 
-parse :: [String] -> [[String]]
+parse :: [String] -> [Valley]
 parse = foldr combineParse [[]]
 
-combineParse :: String -> [[String]] -> [[String]]
+smudge :: Valley -> Int
+smudge valley = (50*) . sum . filter (/=except) $ map (reflectHorizontal . replace valley) [ (row, column) | row <- [0..height-1], column <- [0..width-1] ]
+-- smudge valley = (50*) $ sum . map ((\i -> trace (show i) i) . reflectHorizontal . replace valley) [ (row, column) | row <- [0..height-1], column <- [0..width-1] ]
+    where
+        height = length valley
+        width = length (head valley)
+        except = reflectHorizontal valley
+
+combineParse :: String -> [Valley] -> [Valley]
 combineParse "" acc = [] : acc
 combineParse x (ys:acc) = (x:ys) : acc
 combineParse x [] = error $ "acc is empty. x = " ++ x
 
-replace :: Position -> [String] -> [String]
-replace (row, column) xs = take row xs ++ [newLine] ++ drop (row + 1) xs
+replace :: Valley -> Position -> Valley
+replace xs (row, column) = take row xs ++ [newLine] ++ drop (row + 1) xs
     where
         line = xs !! row
         char = line !! column
