3bb9546990694fcc95475db4509ea11096ced440
Peter van der Heijden <heijp06@gmail.com>
2023-12-13 22:04:30 +0100

    Prepare day 14.

---
+++ 2023/day14/src/Examples.hs
@@ -0,0 +1,19 @@
+module Examples ( example1
+                , example2
+                , result1
+                , result2
+                ) where
+
+example1 :: [String]
+example1 = [
+           ]
+
+result1 :: Int
+result1 = -1
+
+example2 :: [String]
+example2 = example1
+
+result2 :: Int
+result2 = -1
+
