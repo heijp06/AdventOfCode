9a410fee644135eefcef821ef7dbdbb24fb3b4fe
Peter van der Heijden <heijp06@gmail.com>
2023-12-13 08:15:22 +0100

    Solve day 13 part 1.

--- 2023/day13/src/Lib.hs
+++ 2023/day13/src/Lib.hs
@@ -1,19 +1,39 @@
 module Lib
-    ( parse
+    ( reflectHorizontal
+    , reflectVertical
+    , parse
     , part1
     , part2
     ) where
 
+import Data.List (inits, tails, transpose)
+
 part1 :: [String] -> Int
-part1 = undefined
+part1 = sum . map reflect . parse
 
 part2 :: [String] -> Int
 part2 = undefined
 
-parse :: [String] -> [[String]]
-parse = foldr combine [[]]
+reflect :: [String] -> Int
+reflect xs = 100 * reflectHorizontal xs + reflectVertical xs
 
-combine :: String -> [[String]] -> [[String]]
-combine "" acc = [] : acc
-combine x (ys:acc) = (x:ys) : acc
-combine x [] = error $ "acc is empty. x = " ++ x
+reflectHorizontal :: [String] -> Int
+reflectHorizontal xs = case (top, bottom) of
+                        ([], []) -> 0
+                        (_, []) -> length (head top) `div` 2
+                        ([], _) -> length xs - length (head bottom) `div` 2
+                        _ -> error "Reflection from both top and bottom"
+    where
+        top = [ s | s <- inits xs, not $ null s, even $ length s, s == reverse s ]
+        bottom = [ s | s <- tails xs, not $ null s, even $ length s, s == reverse s ]
+
+reflectVertical :: [String] -> Int
+reflectVertical = reflectHorizontal . transpose
+
+parse :: [String] -> [[String]]
+parse = foldr combineParse [[]]
+
+combineParse :: String -> [[String]] -> [[String]]
+combineParse "" acc = [] : acc
+combineParse x (ys:acc) = (x:ys) : acc
+combineParse x [] = error $ "acc is empty. x = " ++ x
