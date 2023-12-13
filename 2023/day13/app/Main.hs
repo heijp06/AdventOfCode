f0a7a19786368f43f1bdc5609019d010b76fc369
Peter van der Heijden <heijp06@gmail.com>
2023-12-13 17:30:14 +0100

    Create separate function for reading the input.

--- 2023/day13/app/Main.hs
+++ 2023/day13/app/Main.hs
@@ -1,12 +1,16 @@
-module Main (main) where
+module Main ( input
+            , main
+            ) where
 
 import Lib
 
 main :: IO ()
 main = do
-    xs <- lines <$> readFile "..\\..\\data\\2023\\day13.txt"
+    xs <- input
     putStr "Part 1: "
     print $ part1 xs
     putStr "Part 2: "
     print $ part2 xs
-
+
+input :: IO [String]
+input = lines <$> readFile "..\\..\\data\\2023\\day13.txt"
--- 2023/day13/src/Lib.hs
+++ 2023/day13/src/Lib.hs
@@ -3,12 +3,14 @@ module Lib
     , parse
     , part1
     , part2
+    , reflect
     , reflectHorizontal
     , reflectVertical
     , replace
     , smudge
     ) where
 
+import Data.Bifunctor (first, second)
 import Data.List (inits, tails, transpose)
 import Text.Printf (printf)
 
@@ -16,33 +18,33 @@ type Position = (Int, Int)
 type Valley = [String]
 
 part1 :: [String] -> Int
-part1 = sum . concatMap reflect . parse
+part1 = sum . concatMap (map snd . reflect) . parse
 
 part2 :: [String] -> Int
 part2 = sum . map smudge . parse
 
-reflect :: Valley -> [Int]
-reflect xs = map (100*) (reflectHorizontal xs) ++ reflectVertical xs
+reflect :: Valley -> [(Valley, Int)]
+reflect xs = map (second (*100)) (reflectHorizontal xs) ++ reflectVertical xs
 
-reflectHorizontal :: Valley -> [Int]
+reflectHorizontal :: Valley -> [(Valley, Int)]
 reflectHorizontal xs = top ++ bottom
     where
-        top = [ length s `div` 2 | s <- inits xs, not $ null s, even $ length s, s == reverse s ]
-        bottom = [ length xs - length s `div` 2 | s <- tails xs, not $ null s, even $ length s, s == reverse s ]
+        top = [ (xs, length s `div` 2) | s <- inits xs, not $ null s, even $ length s, s == reverse s ]
+        bottom = [ (xs, length xs - length s `div` 2) | s <- tails xs, not $ null s, even $ length s, s == reverse s ]
 
-reflectVertical :: Valley -> [Int]
-reflectVertical = reflectHorizontal . transpose
+reflectVertical :: Valley -> [(Valley, Int)]
+reflectVertical = map (first transpose) . reflectHorizontal . transpose
 
 parse :: [String] -> [Valley]
 parse = foldr combineParse [[]]
 
 smudge :: Valley -> Int
-smudge valley = (50*) . sum . filter (/=except) $ concatMap (reflectHorizontal . replace valley) [ (row, column) | row <- [0..height-1], column <- [0..width-1] ]
+smudge valley = (50*) . sum . filter (/=except) $ concatMap (map snd . reflect . replace valley) [ (row, column) | row <- [0..height-1], column <- [0..width-1] ]
     where
         height = length valley
         width = length (head valley)
-        except = case reflectHorizontal valley of
-                    (x:_) -> x
+        except = case reflect valley of
+                    ((_, x):_) -> x
                     _ -> 0
 
 combineParse :: String -> [Valley] -> [Valley]
