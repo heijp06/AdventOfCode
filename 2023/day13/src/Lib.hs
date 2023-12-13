1110aa43f27dc85b1bb5e6230b063d9cc0a3bb61
Peter van der Heijden <peter.vd.heijden@malvernpanalytical.com>
2023-12-13 13:05:12 +0100

    Test for day 13 part 2 passes, answer still wrong.

--- 2023/day13/src/Lib.hs
+++ 2023/day13/src/Lib.hs
@@ -12,43 +12,38 @@ module Lib
 import Data.List (inits, tails, transpose)
 import Text.Printf (printf)
 
-import Debug.Trace (trace)
-
 type Position = (Int, Int)
 type Valley = [String]
 
 part1 :: [String] -> Int
-part1 = sum . map reflect . parse
+part1 = sum . concatMap reflect . parse
 
 part2 :: [String] -> Int
 part2 = sum . map smudge . parse
 
-reflect :: Valley -> Int
-reflect xs = 100 * reflectHorizontal xs + reflectVertical xs
+reflect :: Valley -> [Int]
+reflect xs = map (100*) (reflectHorizontal xs) ++ reflectVertical xs
 
-reflectHorizontal :: Valley -> Int
-reflectHorizontal xs = case (top, bottom) of
-                        ([], []) -> 0
-                        (_, []) -> length (head top) `div` 2
-                        ([], _) -> length xs - length (head bottom) `div` 2
-                        _ -> error $ printf "Reflection from both top and bottom, top = %s, bottom = %s." (show top) (show bottom)
+reflectHorizontal :: Valley -> [Int]
+reflectHorizontal xs = top ++ bottom
     where
-        top = [ s | s <- inits xs, not $ null s, even $ length s, s == reverse s ]
-        bottom = [ s | s <- tails xs, not $ null s, even $ length s, s == reverse s ]
+        top = [ length s `div` 2 | s <- inits xs, not $ null s, even $ length s, s == reverse s ]
+        bottom = [ length xs - length s `div` 2 | s <- tails xs, not $ null s, even $ length s, s == reverse s ]
 
-reflectVertical :: Valley -> Int
+reflectVertical :: Valley -> [Int]
 reflectVertical = reflectHorizontal . transpose
 
 parse :: [String] -> [Valley]
 parse = foldr combineParse [[]]
 
 smudge :: Valley -> Int
-smudge valley = (50*) . sum . filter (/=except) $ map (reflectHorizontal . replace valley) [ (row, column) | row <- [0..height-1], column <- [0..width-1] ]
--- smudge valley = (50*) $ sum . map ((\i -> trace (show i) i) . reflectHorizontal . replace valley) [ (row, column) | row <- [0..height-1], column <- [0..width-1] ]
+smudge valley = (50*) . sum . filter (/=except) $ concatMap (reflectHorizontal . replace valley) [ (row, column) | row <- [0..height-1], column <- [0..width-1] ]
     where
         height = length valley
         width = length (head valley)
-        except = reflectHorizontal valley
+        except = case reflectHorizontal valley of
+                    (x:_) -> x
+                    _ -> 0
 
 combineParse :: String -> [Valley] -> [Valley]
 combineParse "" acc = [] : acc
