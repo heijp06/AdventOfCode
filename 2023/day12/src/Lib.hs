543543ae053c8bb43ff57b5d94f55e75bca2f22a
Peter van der Heijden <heijp06@gmail.com>
2023-12-15 16:58:09 +0100

    Refactor day 12.

--- 2023/day12/src/Lib.hs
+++ 2023/day12/src/Lib.hs
@@ -14,8 +14,8 @@ import Text.Printf (printf)
 
 type Record = (String, [Int])
 
-data MatchState = MatchState { matches :: Int
-                             , state :: Map.Map Record Int
+data MatchState = MatchState { count :: Int
+                             , partialMatches :: Map.Map Record Int
                              } deriving Show
 
 part1 :: [String] -> Int
@@ -25,31 +25,29 @@ part2 :: [String] -> Int
 part2 = sum . map (match . parse 5)
 
 match :: Record -> Int
-match record = matches . head . dropWhile (not . Map.null . state) $ iterate doMatch (MatchState 0 (Map.singleton record 1))
+match record = count . head . dropWhile (not . Map.null . partialMatches) $ iterate doMatch (MatchState 0 (Map.singleton record 1))
 
 doMatch :: MatchState -> MatchState
-doMatch MatchState{..} = foldr combineMatchState (MatchState matches Map.empty) (Map.toList state)
+doMatch MatchState{..} = foldr combineMatchState (MatchState count Map.empty) (Map.toList partialMatches)
 
 combineMatchState :: (Record, Int) -> MatchState -> MatchState
-combineMatchState ((xs, []), n) MatchState{..} | '#' `notElem` xs = MatchState { matches = matches + n, .. }
+combineMatchState ((xs, []), n) MatchState{..} | '#' `notElem` xs = MatchState { count = count + n, .. }
 combineMatchState ((_, []), _) matchState = matchState
-combineMatchState (([], _), _) matchState = matchState
 combineMatchState ((xs, is), _) matchState | length xs < sum is + length is - 1 = matchState
-combineMatchState (('.':xs, is), n) MatchState{..} = MatchState { state = Map.insertWith (+) (xs, is) n state, .. }
+combineMatchState (('.':xs, is), n) MatchState{..} = MatchState { partialMatches = Map.insertWith (+) (xs, is) n partialMatches, .. }
 combineMatchState ((xs@('#':_), i:_), _) matchState | '.' `elem` take i xs = matchState
-combineMatchState ((xs@('#':_), [i]), n) MatchState{..} | length xs == i = MatchState { matches = matches + n, .. }
+combineMatchState ((xs@('?':ys), is@(i:_)), n) MatchState{..} | '.' `elem` take i xs =
+    MatchState { partialMatches = Map.insertWith (+) (ys, is) n partialMatches, .. }
+combineMatchState ((xs, [i]), n) MatchState{..} | length xs == i = MatchState { count = count + n, .. }
 combineMatchState ((xs@('#':_), i:_), _) matchState | xs !! i == '#' = matchState
 combineMatchState ((xs@('#':_), i:js), n) MatchState{..} =
-    MatchState { state = Map.insertWith (+) (drop (i + 1) xs, js) n state, .. }
-combineMatchState ((xs@('?':ys), is@(i:_)), n) MatchState{..} | '.' `elem` take i xs =
-    MatchState { state = Map.insertWith (+) (ys, is) n state, .. }
-combineMatchState ((xs@('?':_), [i]), n) MatchState{..} | length xs == i = MatchState { matches = matches + n, .. }
+    MatchState { partialMatches = Map.insertWith (+) (drop (i + 1) xs, js) n partialMatches, .. }
 combineMatchState ((xs@('?':ys), is@(i:_)), n) MatchState{..} | xs !! i == '#' =
-    MatchState { state = Map.insertWith (+) (ys, is) n state, .. }
-combineMatchState ((xs@('?':ys), is@(i:js)), n) MatchState{..} = MatchState { state = state2, .. }
+    MatchState { partialMatches = Map.insertWith (+) (ys, is) n partialMatches, .. }
+combineMatchState ((xs@('?':ys), is@(i:js)), n) MatchState{..} = MatchState { partialMatches = partialMacthes2, .. }
     where
-        state1 = Map.insertWith (+) (ys, is) n state
-        state2 = Map.insertWith (+) (drop (i + 1) xs, js) n state1
+        partialMacthes1 = Map.insertWith (+) (ys, is) n partialMatches
+        partialMacthes2 = Map.insertWith (+) (drop (i + 1) xs, js) n partialMacthes1
 combineMatchState record matchState = error $ printf "Unhandled case: '%s' %s" (show record) (show matchState)
 
 parse :: Int -> String -> (String, [Int])
