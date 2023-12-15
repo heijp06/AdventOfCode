23e15d21d5bb3d8c72dbda78aa3b1e35efc21345
Peter van der Heijden <heijp06@gmail.com>
2023-12-15 20:42:37 +0100

    Refactor day 12.

--- 2023/day14/src/Lib.hs
+++ 2023/day14/src/Lib.hs
@@ -2,47 +2,69 @@
 
 module Lib
     ( cubesByColumns
-    , fall
+    , cubesByRows
+    , cycle
+    , positionsOfRocks
     , part1
     , part2
-    , rocksByColumns
-    , tilt
+    , tiltNorth
+    , tiltWest
+    , tiltSouth
+    , tiltEast
     ) where
 
 import Data.List (find)
 import Data.Maybe (fromJust)
+import Data.Map ((!))
 import qualified Data.Map as Map
+import Prelude hiding (cycle)
 
 type Position = (Int, Int)
 
 part1 :: [String] -> Int
-part1 xs = sum . map (height-) . concat $ tilt cubes rocks
+part1 xs = sum . map ((height-) . fst) $ tiltNorth cubes rocks
     where
         cubes = cubesByColumns xs
-        rocks = rocksByColumns xs
+        rocks = positionsOfRocks xs
         height = length xs
 
 part2 :: [String] -> Int
 part2 = undefined
 
--- columnsToRows :: Int -> [[Int]] -> [[Int]]
--- columnsToRows height iss = undefined
---     where
---         rows = Map.fromList $ map (,[-1]) [0..height-1]
---         x = foldl addColumn rows $ zip [0..] iss
---         addColumn r (col, rs) = 
+cycle :: Map.Map Int [Int] -> Map.Map Int [Int] -> [Position] -> [Position]
+cycle cubeCols cubeRows rocks = rocksEast
+    where
+        rocksNorth = tiltNorth cubeCols rocks
+        rocksWest = tiltWest cubeRows rocksNorth
+        rocksSouth = tiltSouth cubeCols rocksWest
+        rocksEast = tiltEast cubeRows rocksSouth
 
-tilt :: [[Int]] -> [[Int]] -> [[Int]]
-tilt = zipWith fall
-
-fall :: [Int] -> [Int] -> [Int]
-fall cubes rocks = concatMap (\ (r, n) -> [r+1..r+n]) $ Map.toAscList m
+tiltNorth :: Map.Map Int [Int] -> [Position] -> [Position]
+tiltNorth cubes rocks = concatMap (\ ((r, c), n) -> map (,c) [r+1..r+n]) $ Map.toAscList m
     where
         m = foldr f Map.empty rocks
-        f rock = Map.insertWith (+) (fromJust $ find (<rock) cubes) 1
+        f (row, col) = Map.insertWith (+) (fromJust $ find (<row) (cubes ! col), col) 1
 
-cubesByColumns :: [String] -> [[Int]]
-cubesByColumns xs = map ((width:) . snd) . Map.toAscList . foldl addCube columns $
+tiltWest :: Map.Map Int [Int] -> [Position] -> [Position]
+tiltWest cubes rocks = concatMap (\ ((r, c), n) -> map (r,) [c+1..c+n]) $ Map.toAscList m
+    where
+        m = foldr f Map.empty rocks
+        f (row, col) = Map.insertWith (+) (row, fromJust $ find (<col) (cubes ! row)) 1
+
+tiltSouth :: Map.Map Int [Int] -> [Position] -> [Position]
+tiltSouth cubes rocks = concatMap (\ ((r, c), n) -> map (,c) [r-n..r-1]) $ Map.toAscList m
+    where
+        m = foldr f Map.empty rocks
+        f (row, col) = Map.insertWith (+) (fromJust $ find (>row) (reverse (cubes ! col)), col) 1
+
+tiltEast :: Map.Map Int [Int] -> [Position] -> [Position]
+tiltEast cubes rocks = concatMap (\ ((r, c), n) -> map (r,) [c-n..c-1]) $ Map.toAscList m
+    where
+        m = foldr f Map.empty rocks
+        f (row, col) = Map.insertWith (+) (row, fromJust $ find (>col) (reverse (cubes ! row))) 1
+
+cubesByColumns :: [String] -> Map.Map Int [Int]
+cubesByColumns xs = Map.map (height:) . foldl addCube columns $
             zip [ (row, column) | row <- [0..height-1], column <- [0..width-1] ] (concat xs)
     where
         height = length xs
@@ -53,14 +75,20 @@ addCube :: Map.Map Int [Int] -> (Position, Char) -> Map.Map Int [Int]
 addCube acc ((row, column), '#') = Map.insertWith (++) column [row] acc
 addCube acc _ = acc
 
-rocksByColumns :: [String] -> [[Int]]
-rocksByColumns xs = map snd . Map.toAscList . foldl addRock columns $
+cubesByRows :: [String] -> Map.Map Int [Int]
+cubesByRows xs = Map.map (width:) . foldl addCube' rows $
             zip [ (row, column) | row <- [0..height-1], column <- [0..width-1] ] (concat xs)
     where
         height = length xs
         width = length $ head xs
-        columns = Map.fromList $ map (,[]) [0..width-1]
+        rows = Map.fromList $ map (,[-1]) [0..height-1]
 
-addRock :: Map.Map Int [Int] -> (Position, Char) -> Map.Map Int [Int]
-addRock acc ((row, column), 'O') = Map.insertWith (++) column [row] acc
-addRock acc _ = acc
+addCube' :: Map.Map Int [Int] -> (Position, Char) -> Map.Map Int [Int]
+addCube' acc ((row, column), '#') = Map.insertWith (++) row [column] acc
+addCube' acc _ = acc
+
+positionsOfRocks :: [String] -> [Position]
+positionsOfRocks xs = map fst . filter ((=='O') . snd) $ zip [ (row, column) | row <- [0..height-1], column <- [0..width-1] ] (concat xs)
+    where
+        height = length xs
+        width = length $ head xs
