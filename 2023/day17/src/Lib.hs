58cacd798b36f73995ee9f30348142fd14edf807
Peter van der Heijden <heijp06@gmail.com>
2023-12-17 07:23:30 +0100

    Start day 17.

---
+++ 2023/day17/src/Lib.hs
@@ -0,0 +1,17 @@
+module Lib
+    ( part1
+    , part2
+    , parse
+    ) where
+
+import qualified Data.Array as Array
+import Data.Ix (inRange, range)
+
+part1 :: [String] -> Int
+part1 = undefined
+
+part2 :: [String] -> Int
+part2 = undefined
+
+parse :: [String] -> Array.Array (Int, Int) Int
+parse = undefined
