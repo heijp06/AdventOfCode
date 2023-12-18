61e45f848953244d7da60fed2743c94db5f8390e
Peter van der Heijden <peter.vd.heijden@malvernpanalytical.com>
2023-12-18 07:24:08 +0100

    Continue day 18 part 1.

--- 2023/day18/src/Lib.hs
+++ 2023/day18/src/Lib.hs
@@ -1,13 +1,17 @@
 module Lib
-    ( parse
+    ( fill
+    , outline
+    , parse
     , part1
     , part2
     ) where
 
+import Data.Ix (range)
 import qualified Data.Set as Set
 
 type Position = (Int, Int)
 type Direction = (Int, Int)
+data ScanState = Out | In | OutBorderUp | OutBorderDown | InBorderUp | InBorderDown
 
 part1 :: [String] -> Int
 part1 = undefined
@@ -18,17 +22,40 @@ part2 = undefined
 parse :: [String] -> [(Direction, Int)]
 parse = map parseLine
 
+fill :: Set.Set Position -> Set.Set Position
+fill positions = snd . foldl combineFill (Out, positions) $ range ((minRow, minColumn), (maxRow, maxColumn))
+    where
+        minRow = minimum (Set.map fst positions) - 1
+        minColumn = minimum (Set.map snd positions) - 1
+        maxRow = maximum (Set.map fst positions) + 1
+        maxColumn = maximum (Set.map snd positions) + 1
+
+combineFill :: (ScanState, Set.Set Position) -> Position -> (ScanState, Set.Set Position)
+combineFill (Out, positions) position@(row, column) | position `Set.member` positions =
+    if (row - 1, column) `Set.member` positions
+        then
+            if (row + 1, column) `Set.member` positions
+                then (In, positions)
+                else (OutBorderUp, positions)
+        else
+            if (row + 1, column) `Set.member` positions
+                then (OutBorderDown, positions)
+                else error "On border, but no border up or down."
+combineFill acc@(Out, _) _ = acc
+combineFill _ _ = undefined
+
 outline :: [(Direction, Int)] -> Set.Set Position
 outline = snd . foldl combineOutline ((0, 0), Set.singleton (0, 0))
 
 combineOutline :: (Position, Set.Set Position) -> (Direction, Int) -> (Position, Set.Set Position)
-combineOutline = undefined
+combineOutline (position, loop) (dir, n) =
+    (move position (n `mul` dir), Set.union loop (Set.fromList [move position (i `mul` dir) | i <- [1..n] ]))
 
 move :: Position -> Direction -> Position
 move (row, column) (dRow, dColumn) = (row + dRow, column + dColumn)
 
-mul :: Direction -> Int -> Direction
-mul (dRow, dColumn) n = (n * dRow, n * dColumn)
+mul :: Int -> Direction -> Direction
+mul n (dRow, dColumn) = (n * dRow, n * dColumn)
         
 parseLine :: String -> (Direction, Int)
 parseLine xs = case words xs of
