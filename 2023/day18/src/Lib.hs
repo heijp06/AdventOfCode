24a691451a16c1a54fa783a13c79de6c6e2f2080
Peter van der Heijden <heijp06@gmail.com>
2023-12-19 20:23:34 +0100

    Try different aproach for day 18 part 1.

--- 2023/day18/src/Lib.hs
+++ 2023/day18/src/Lib.hs
@@ -1,5 +1,6 @@
 module Lib
-    ( fill
+    ( bounds
+    , fillAround
     , outline
     , parse
     , part1
@@ -15,7 +16,9 @@ type Direction = (Int, Int)
 data ScanState = Out | In | OutBorderUp | OutBorderDown | InBorderUp | InBorderDown
 
 part1 :: [String] -> Int
-part1 = undefined
+part1 xs = undefined
+    where
+        loop = outline $ parse xs
 
 part2 :: [String] -> Int
 part2 = undefined
@@ -32,31 +35,40 @@ rows xs = [ [ if (row, column) `Set.member` positions then '#' else ' ' | column
         maxRow = maximum (Set.map fst positions) + 1
         maxColumn = maximum (Set.map snd positions) + 1
 
-fill :: Set.Set Position -> Set.Set Position
-fill positions = snd . foldl combineFill (Out, positions) $ range ((minRow, minColumn), (maxRow, maxColumn))
+bounds :: Set.Set Position -> (Position, Position)
+bounds positions = ((minRow, minColumn), (maxRow, maxColumn))
     where
-        minRow = minimum (Set.map fst positions) - 1
-        minColumn = minimum (Set.map snd positions) - 1
-        maxRow = maximum (Set.map fst positions) + 1
-        maxColumn = maximum (Set.map snd positions) + 1
+        minRow = minimum (Set.map fst positions)
+        minColumn = minimum (Set.map snd positions)
+        maxRow = maximum (Set.map fst positions)
+        maxColumn = maximum (Set.map snd positions)
 
-combineFill :: (ScanState, Set.Set Position) -> Position -> (ScanState, Set.Set Position)
-combineFill (Out, positions) position@(row, column) | position `Set.member` positions =
-    if (row + 1, column) `Set.member` positions
-        then (OutBorderDown, positions)
-        else (OutBorderUp, positions)
-combineFill acc@(Out, _) _ = acc
-combineFill (In, positions) position@(row, column) | position `Set.member` positions =
-    if (row + 1, column) `Set.member` positions
-        then (InBorderDown, positions)
-        else (InBorderUp, positions)
-combineFill (In, positions) position = (In, Set.insert position positions)
-combineFill (OutBorderUp, positions) position@(row, column) | position `Set.member` positions =
-    if (row + 1, column) `Set.member` positions
-        then (In, positions)
-        else (OutBorderUp, positions)
-combineFill (OutBorderUp, positions) position = (In, positions)
-combineFill _ _ = undefined
+fillAround :: Set.Set Position -> Set.Set Position
+fillAround loop = fst 
+                . head
+                . dropWhile (not . Set.null . snd)
+                $ iterate doFill (seen, Set.singleton (minRow + 1, minColumn + 1))
+    where
+        minRow = minimum (Set.map fst loop) - 2
+        minColumn = minimum (Set.map snd loop) - 2
+        maxRow = maximum (Set.map fst loop) + 2
+        maxColumn = maximum (Set.map snd loop) + 2
+        seen = Set.unions [ loop
+                            , Set.fromList $ range ((minRow, minColumn), (minRow, maxColumn))
+                            , Set.fromList $ range ((minRow, minColumn), (maxRow, minColumn))
+                            , Set.fromList $ range ((minRow, maxColumn), (maxRow, maxColumn))
+                            , Set.fromList $ range ((maxRow, minColumn), (maxRow, maxColumn))
+                            ]
+
+doFill :: (Set.Set Position, Set.Set Position) -> (Set.Set Position, Set.Set Position)
+doFill (seen, current) = (both, new)
+    where
+        both = Set.union seen current
+        new = Set.fromList [ (r + dr, c + dc)
+                           | (r, c) <- Set.toList current
+                           , (dr, dc) <- [(1, 0), (-1, 0), (0, 1), (0, -1)]
+                           , (r + dr, c + dc) `Set.notMember` both
+                           ]
 
 outline :: [(Direction, Int)] -> Set.Set Position
 outline = snd . foldl combineOutline ((0, 0), Set.singleton (0, 0))
@@ -83,3 +95,33 @@ direction "L" = (0, -1)
 direction "R" = (0, 1)
 direction xs = error $ "Unknown direction " ++ xs
         
+
+-- fill :: Set.Set Position -> Set.Set Position
+-- fill positions = snd . foldl combineFill (Out, positions) $ range ((minRow, minColumn), (maxRow, maxColumn))
+--     where
+--         minRow = minimum (Set.map fst positions) - 1
+--         minColumn = minimum (Set.map snd positions) - 1
+--         maxRow = maximum (Set.map fst positions) + 1
+--         maxColumn = maximum (Set.map snd positions) + 1
+
+-- combineFill :: (ScanState, Set.Set Position) -> Position -> (ScanState, Set.Set Position)
+-- combineFill (Out, positions) position@(row, column) | position `Set.member` positions =
+--     if (row + 1, column) `Set.member` positions
+--         then (OutBorderDown, positions)
+--         else (OutBorderUp, positions)
+-- combineFill acc@(Out, _) _ = acc
+-- combineFill (In, positions) position@(row, column) | position `Set.member` positions =
+--     if (row + 1, column) `Set.member` positions
+--         then (InBorderDown, positions)
+--         else (InBorderUp, positions)
+-- combineFill (In, positions) position = (In, Set.insert position positions)
+-- combineFill (OutBorderUp, positions) position@(row, column) | position `Set.member` positions =
+--     if (row + 1, column) `Set.member` positions
+--         then (In, positions)
+--         else (OutBorderUp, positions)
+-- combineFill (OutBorderUp, positions) position = (In, positions)
+-- combineFill (OutBorderDown, positions) position@(row, column) | position `Set.member` positions =
+--     if (row + 1, column) `Set.member` positions
+--         then (Out, positions)
+--         else (OutBorderDown, positions)
+-- combineFill _ _ = undefined
