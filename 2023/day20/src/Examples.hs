a9655b7132b074445f0806bf1d70da5eeb249126
Peter van der Heijden <heijp06@gmail.com>
2023-12-20 12:13:16 +0100

    Start day 20 part 1.

--- 2023/day20/src/Examples.hs
+++ 2023/day20/src/Examples.hs
@@ -1,19 +1,36 @@
-module Examples ( example1
+module Examples ( example1a
+                , example1b
                 , example2
-                , result1
+                , result1a
+                , result1b
                 , result2
                 ) where
 
-example1 :: [String]
-example1 = [
+example1a :: [String]
+example1a = [ "broadcaster -> a, b, c"
+           , "%a -> b"
+           , "%b -> c"
+           , "%c -> inv"
+           , "&inv -> a"
            ]
 
-result1 :: Int
-result1 = -1
+result1a :: Int
+result1a = 32000000
+
+example1b :: [String]
+example1b = [ "broadcaster -> a"
+            , "%a -> inv, con"
+            , "&inv -> b"
+            , "%b -> con"
+            , "&con -> output"
+            ]
+
+result1b :: Int
+result1b = 11687500
 
 example2 :: [String]
-example2 = example1
+example2 = example1a
 
 result2 :: Int
 result2 = -1
-
+
--- 2023/day20/test/Spec.hs
+++ 2023/day20/test/Spec.hs
@@ -7,8 +7,9 @@ import Examples
 main :: IO ()
 main = hspecWith defaultConfig {configFailFast = True} $ do
     describe "part1" $ do
-        it "part1 example1" $ part1 example1 `shouldBe` result1
+        it "part1 example1a" $ part1 example1a `shouldBe` result1a
+        it "part1 example1b" $ part1 example1b `shouldBe` result1b
 
     describe "part2" $ do
         it "part2 example2" $ part2 example2 `shouldBe` result2
-
+
