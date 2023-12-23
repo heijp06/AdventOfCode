81ad42eaa43c9d8ad84cda5ce77ebd3025fba49d
Peter van der Heijden <heijp06@gmail.com>
2023-12-23 19:37:06 +0100

    Refactor day 22.

--- 2023/day22/src/Examples.hs
+++ 2023/day22/src/Examples.hs
@@ -21,5 +21,5 @@ example2 :: [String]
 example2 = example1
 
 result2 :: Int
-result2 = -1
+result2 = 7
 
--- 2023/day22/src/Lib.hs
+++ 2023/day22/src/Lib.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TupleSections, RecordWildCards #-}
 
 module Lib
     ( fall
@@ -13,18 +13,34 @@ import Data.List.Split (splitOn)
 import qualified Data.Set as Set
 
 type Cube = (Int, Int, Int)
-type Brick = Set.Set Cube
+data Brick = Brick { cubes :: Set.Set Cube
+                   , fallen :: Bool
+                   } deriving Show
+
+instance Eq Brick where
+    b1 == b2 = cubes b1 == cubes b2
+
+instance Ord Brick where
+    b1 `compare` b2 = cubes b1 `compare` cubes b2
 
 part1 :: [String] -> Int
 part1 xs = number - Set.size supporting
     where
         number = length xs
         bricks = parse xs
-        fallen = fall bricks
-        supporting = supports fallen
+        supporting = supports $ fall bricks
 
 part2 :: [String] -> Int
-part2 = undefined
+part2 xs = sum $ Set.map (disintegrate stable) supporting
+    where
+        bricks = parse xs
+        stable = Set.map (\ Brick{..} -> Brick { fallen = False, .. }) $ fall bricks
+        supporting = supports stable
+
+disintegrate :: Set.Set Brick -> Brick -> Int
+disintegrate bricks brick = Set.size . Set.filter fallen $ fall newBricks
+    where
+        newBricks = Set.delete brick bricks
 
 supports :: Set.Set Brick -> Set.Set Brick
 supports bricks = foldr (combineSupports bricks) Set.empty bricks
@@ -36,13 +52,15 @@ combineSupports bricks brick acc =
         else acc
     where
         newBricks = Set.delete brick bricks
-        newBrick = Set.map (\ (x, y, z) -> (x, y, z - 1)) brick
-        supporting = Set.filter (not . Set.disjoint newBrick) newBricks
+        newBrick = Brick { cubes = Set.map (\ (x, y, z) -> (x, y, z - 1)) (cubes brick)
+                         , fallen = True
+                         }
+        supporting = Set.filter (not . Set.disjoint (cubes newBrick) . cubes) newBricks
 
 fall :: Set.Set Brick -> Set.Set Brick
 fall bricks = getBricks . head . dropWhile continue $ iterate step (allCubes, bricks, True)
     where
-        allCubes = foldr1 Set.union bricks
+        allCubes = foldr1 Set.union $ Set.map cubes bricks
         continue (_, _, changed) = changed
         getBricks (_, b, _) = b
 
@@ -50,14 +68,14 @@ step :: (Set.Set Cube, Set.Set Brick, Bool) -> (Set.Set Cube, Set.Set Brick, Boo
 step (allCubes, allBricks, _) = foldr combineBricks (allCubes, allBricks, False) allBricks
 
 combineBricks :: Brick -> (Set.Set Cube, Set.Set Brick, Bool) -> (Set.Set Cube, Set.Set Brick, Bool)
-combineBricks brick acc | any (\ (_, _, z) -> z <= 1) brick = acc
+combineBricks brick acc | any (\ (_, _, z) -> z <= 1) (cubes brick) = acc
 combineBricks brick acc@(allCubes, allBricks, _) =
     if Set.disjoint newCubes newBrick
-        then (Set.union newCubes newBrick, Set.insert newBrick newBricks, True)
+        then (Set.union newCubes newBrick, Set.insert (Brick newBrick True) newBricks, True)
         else acc
     where
-        newBrick = Set.map (\ (x, y, z) -> (x, y, z - 1)) brick
-        newCubes = Set.difference allCubes brick
+        newBrick = Set.map (\ (x, y, z) -> (x, y, z - 1)) (cubes brick)
+        newCubes = Set.difference allCubes (cubes brick)
         newBricks = Set.delete brick allBricks
 
 parse :: [String] -> Set.Set Brick
@@ -65,9 +83,9 @@ parse = Set.fromList . map createBrick
 
 createBrick :: String -> Brick
 createBrick xs = case (start, end) of
-        ((x1, y1, z1), (x2, y2, z2)) | (x1, y1) == (x2, y2) -> Set.fromList $ map (x1,y1,) [z1..z2]
-        ((x1, y1, z1), (x2, y2, z2)) | (x1, z1) == (x2, z2) -> Set.fromList $ map (x1,,z1) [y1..y2]
-        ((x1, y1, z1), (x2, y2, z2)) | (y1, z1) == (y2, z2) -> Set.fromList $ map (,y1,z1) [x1..x2]
+        ((x1, y1, z1), (x2, y2, z2)) | (x1, y1) == (x2, y2) -> Brick (Set.fromList $ map (x1,y1,) [z1..z2]) False
+        ((x1, y1, z1), (x2, y2, z2)) | (x1, z1) == (x2, z2) -> Brick (Set.fromList $ map (x1,,z1) [y1..y2]) False
+        ((x1, y1, z1), (x2, y2, z2)) | (y1, z1) == (y2, z2) -> Brick (Set.fromList $ map (,y1,z1) [x1..x2]) False
         _ -> error $ "Unhandled pair: " ++ show (start, end)
     where
         (left, right) = splitInTwo xs
