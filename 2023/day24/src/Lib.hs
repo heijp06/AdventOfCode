f8e45b166f894c251e41bbe692e89468e294a855
Peter van der Heijden <heijp06@gmail.com>
2023-12-25 08:29:23 +0100

    Solve day 24 part 1.

--- 2023/day24/src/Lib.hs
+++ 2023/day24/src/Lib.hs
@@ -7,9 +7,6 @@ module Lib
 
 import Data.List (tails)
 
-import Debug.Trace (trace)
-import Text.Printf (printf)
-
 type Position = (Int, Int, Int)
 type Velocity = (Int, Int, Int)
 type Stone = (Position, Velocity)
@@ -29,31 +26,16 @@ solve bounds xs =
 intersects :: (Double, Double) -> (Stone, Stone) -> Bool
 intersects _ (((x1, y1, _), (vx1, vy1, _)), ((x2, y2, _), (vx2, vy2, _)))
     | x1 == x2 && y1 == y2 && vx1 * vy2 == vx2 * vy1 = True
-intersects _ (((x1, _, _), (vx1, _, _)), ((x2, _, _), (vx2, _, _)))
-    | x1 == x2 && vx1 /= vx2 = False
-intersects _ (((_, y1, _), (_, vy1, _)), ((_, y2, _), (_, vy2, _)))
-    | y1 == y2 && vy1 /= vy2 = False
-intersects _ (((x1, _ , _), (vx1, _, _)), ((x2, _ , _), (vx2, _, _)))
-    | x1 /= x2 && vx1 == vx2 = False
-intersects _ (((_, y1, _), (_, vy1, _)), ((_, y2, _), (_, vy2, _)))
-    | y1 /= y2 && vy1 == vy2 = False
+intersects _ ((_, (vx1, vy1, _)), (_, (vx2, vy2, _)))
+    | vx1 * vy2 == vx2 * vy1 = False
 intersects (lbound, ubound) (((x1, y1, _), (vx1, vy1, _)), ((x2, y2, _), (vx2, vy2, _)))
-    | x1 == x2 && vx1 == vx2 = t > 0 && y >= lbound && y <= ubound
+    = t1 > 0 && t2 > 0 && x >= lbound && x <= ubound && y >= lbound && y <= ubound
         where
-            t = fromIntegral (y2 - y1) / fromIntegral (vy1 - vy2)
-            y = fromIntegral y1 + t * fromIntegral vy1
-intersects (lbound, ubound) (((x1, y1, _), (vx1, vy1, _)), ((x2, y2, _), (vx2, vy2, _)))
-    | y1 == y2 && vy1 == vy2 = t > 0 && x >= lbound && x <= ubound
-        where
-            t = fromIntegral (x2 - x1) / fromIntegral (vx1 - vx2)
-            x = fromIntegral x1 + t * fromIntegral vx1
-intersects (lbound, ubound) (((x1, y1, _), (vx1, vy1, _)), ((x2, y2, _), (vx2, vy2, _)))
-    = tx > 0 && x >= lbound && x <= ubound && ty > 0 && y >= lbound && y <= ubound
-        where
-            tx = fromIntegral (x2 - x1) / fromIntegral (vx1 - vx2)
-            x = fromIntegral x1 + tx * fromIntegral vx1
-            ty = fromIntegral (y2 - y1) / fromIntegral (vy1 - vy2)
-            y = fromIntegral y1 + ty * fromIntegral vy1
+            x = fromIntegral x1 + t1 * fromIntegral vx1
+            y = fromIntegral y1 + t1 * fromIntegral vy1
+            t2 = fromIntegral (vx1 * (y2 - y1) - vy1 * (x2 - x1)) /
+                fromIntegral (vy1 * vx2 - vx1 * vy2)
+            t1 = (fromIntegral (x2 - x1) + fromIntegral vx2 * t2) / fromIntegral vx1
 
 parse :: String -> (Position, Velocity)
 parse xs = case words xs of
