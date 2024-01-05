10cc8d151917fa7e6a59a3be3c8b054c68278444
Peter van der Heijden <heijp06@gmail.com>
2024-01-05 16:31:30 +0100

    Add batch file for running all days.

---
+++ 2023/runall.bat
@@ -0,0 +1,127 @@
+@echo off
+
+cd day01
+echo Day 1
+stack run 2>nul
+cd ..
+
+cd day02
+echo Day 2
+stack run 2>nul
+cd ..
+
+cd day03
+echo Day 3
+stack run 2>nul
+cd ..
+
+cd day04
+echo Day 4
+stack run 2>nul
+cd ..
+
+cd day05
+echo Day 5
+stack run 2>nul
+cd ..
+
+cd day06
+echo Day 6
+stack run 2>nul
+cd ..
+
+cd day07
+echo Day 7
+stack run 2>nul
+cd ..
+
+cd day08
+echo Day 8
+stack run 2>nul
+cd ..
+
+cd day09
+echo Day 9
+stack run 2>nul
+cd ..
+
+cd day10
+echo Day 10
+stack run 2>nul
+cd ..
+
+cd day11
+echo Day 11
+stack run 2>nul
+cd ..
+
+cd day12
+echo Day 12
+stack run 2>nul
+cd ..
+
+cd day13
+echo Day 13
+stack run 2>nul
+cd ..
+
+cd day14
+echo Day 14
+stack run 2>nul
+cd ..
+
+cd day15
+echo Day 15
+stack run 2>nul
+cd ..
+
+cd day16
+echo Day 16
+stack run 2>nul
+cd ..
+
+cd day17
+echo Day 17
+stack run 2>nul
+cd ..
+
+cd day18
+echo Day 18
+stack run 2>nul
+cd ..
+
+cd day19
+echo Day 19
+stack run 2>nul
+cd ..
+
+cd day20
+echo Day 20
+stack run 2>nul
+cd ..
+
+cd day21
+echo Day 21
+stack run 2>nul
+cd ..
+
+cd day22
+echo Day 22
+stack run 2>nul
+cd ..
+
+cd day23
+echo Day 23
+stack run 2>nul
+cd ..
+
+cd day24
+echo Day 24
+stack run 2>nul
+cd ..
+
+cd day25
+echo Day 25
+stack run 2>nul
+cd ..
+
