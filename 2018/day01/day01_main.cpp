86b86f927d0517166512172ac13ab7e3d930d86c
Peter van der Heijden <heijp06@gmail.com>
2024-11-16 12:46:03 +0100

    Add main executable.

---
+++ 2018/day01/day01_main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "day01.h"
+
+int main() {
+	std::cout << "Part 1: " << day01::part1({}) << std::endl;
+	std::cout << "Part 2: " << day01::part2({}) << std::endl;
+
+	return 0;
+}
