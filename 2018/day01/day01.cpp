8391289126691d7253c7f3a32e6f31a74673820b
Peter van der Heijden <heijp06@gmail.com>
2024-11-16 12:52:51 +0100

    Ignore rows argument.

--- 2018/day01/day01.cpp
+++ 2018/day01/day01.cpp
@@ -2,10 +2,12 @@
 
 namespace day01 {
     int part1(std::vector<std::string> rows) {
+        (void)rows;
         return -1;
     }
 
     int part2(std::vector<std::string> rows) {
+        (void)rows;
         return -1;
     }
 }
