5bc10b41a80ec29e5dc9c59ddd3aa794a2717d97
Peter van der Heijden <heijp06@gmail.com>
2024-11-16 21:57:36 +0100

    Start day 2.

---
+++ 2018/day02/day02.cpp
@@ -0,0 +1,13 @@
+#include "day02.h"
+
+namespace day02 {
+    int part1(std::vector<std::string> rows) {
+        (void)rows;
+        return -1;
+    }
+
+    int part2(std::vector<std::string> rows) {
+        (void)rows;
+        return -1;
+    }
+}
