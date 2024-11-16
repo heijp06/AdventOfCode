a4809c53cb9dacef02d461a83a5e071dd7dfaba7
Peter van der Heijden <heijp06@gmail.com>
2024-11-16 21:27:06 +0100

    Refactor, add constant for AoC-data, simplify calculation of path.

--- 2018/lib/advent.h
+++ 2018/lib/advent.h
@@ -24,25 +24,24 @@ namespace advent {
 		return rows;
 	}
 
-	// Get the path to the input file for @year and @day as a vector of strings
+	// Get the path to the input file for @year and @day
 	std::string get_data_path(int year, int day) {
-		auto current{std::filesystem::current_path()};
+		const std::string data_folder{"AoC-data"};
 		auto year_folder{std::to_string(year)};
 		std::ostringstream ss;
 		ss << "day" << std::setw(2) << std::setfill('0') << day << ".txt";
 		std::string day_file(ss.str());
 
-		while (!std::filesystem::exists(current / "AoC-data")) {
+		auto current{std::filesystem::current_path()};
+		while (!std::filesystem::exists(current / data_folder)) {
 			if (!current.has_parent_path()) {
 				throw std::domain_error("Cannot find data.");
 			}
 			current = current.parent_path();
 		}
 
-		current /= "AoC-data";
-		current /= year_folder;
-		current /= day_file;
+		auto input = current / data_folder / year_folder / day_file;
 
-		return current.string();
+		return input.string();
 	}
 }
