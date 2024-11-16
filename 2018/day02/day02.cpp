e45f82611e91b612b533fa08468f5ab5f747e94b
Peter van der Heijden <heijp06@gmail.com>
2024-11-16 22:20:06 +0100

    Solve day 2 part 1.

--- 2018/day02/day02.cpp
+++ 2018/day02/day02.cpp
@@ -1,9 +1,33 @@
 #include "day02.h"
 
+#include <map>
+
 namespace day02 {
     int part1(std::vector<std::string> rows) {
-        (void)rows;
-        return -1;
+        auto twice{0};
+        auto thrice{0};
+
+        for (const auto& row : rows) {
+			std::map<char, int> counts{};
+            for (auto c : row) {
+                counts[c]++;
+            }
+
+            auto has_twice{false};
+            auto has_thrice{false};
+            for (auto it = counts.cbegin(); it != counts.cend(); it++) {
+                if (it->second == 2) {
+                    has_twice = true;
+                }
+                else if (it->second == 3) {
+                    has_thrice = true;
+                }
+            }
+            twice += has_twice;
+            thrice += has_thrice;
+        }
+
+        return twice * thrice;
     }
 
     int part2(std::vector<std::string> rows) {
--- 2018/day02/day02_test.cpp
+++ 2018/day02/day02_test.cpp
@@ -1,6 +1,19 @@
 #include "../lib/catch.hpp"
 #include "day02.h"
 
+#include <string>
+#include <vector>
+
 TEST_CASE("part1") {
-    REQUIRE(true);
+	std::vector<std::string> ids = {
+		"abcdef",
+		"bababc",
+		"abbcde",
+		"abcccd",
+		"aabcdd",
+		"abcdee",
+		"ababab"
+	};
+
+    REQUIRE(day02::part1(ids) == 12);
 }
