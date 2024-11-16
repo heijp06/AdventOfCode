77c20dbd3283f2b7fd51e796ac2c936f5c5f8404
Peter van der Heijden <heijp06@gmail.com>
2024-11-16 14:02:05 +0100

    Solve day01 part 1.

--- 2018/day01/day01.cpp
+++ 2018/day01/day01.cpp
@@ -1,9 +1,19 @@
+#include <sstream>
+
 #include "day01.h"
 
 namespace day01 {
     int part1(std::vector<std::string> rows) {
-        (void)rows;
-        return -1;
+        int sum = 0;
+
+        for (auto row : rows) {
+            int value;
+            std::istringstream rs{row};
+            rs >> value;
+            sum += value;
+        }
+
+        return sum;
     }
 
     int part2(std::vector<std::string> rows) {
