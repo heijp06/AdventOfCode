4e5d8a496c82bfed607354fbb41ce483434ef3c2
Peter van der Heijden <heijp06@gmail.com>
2024-11-16 12:23:09 +0100

    Initial version compiles.

---
+++ 2018/day01/day01.cpp
@@ -0,0 +1,11 @@
+#include "day01.h"
+
+namespace day01 {
+    int part1(std::vector<std::string> rows) {
+        return -1;
+    }
+
+    int part2(std::vector<std::string> rows) {
+        return -1;
+    }
+}
