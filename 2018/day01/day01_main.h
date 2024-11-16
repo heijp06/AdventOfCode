7db9aecf328f6f29175bc84568536c9c2bf7888e
Peter van der Heijden <heijp06@gmail.com>
2024-11-16 21:15:45 +0100

    Create advent library and use it.

--- 2018/day01/day01_main.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <string>
-#include <vector>
-
-std::vector<std::string> get_rows(const std::string& path);
---
+++ 2018/lib/advent.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <filesystem>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+namespace advent {
+	std::vector<std::string> get_rows(int year, int day);
+	std::string get_data_path(int year, int day);
+
+	// Read the input file for @year and @day as a vector of strings
+	std::vector<std::string> get_rows(int year, int day) {
+		auto path{get_data_path(year, day)};
+		std::vector<std::string> rows{};
+		std::string row;
+		std::ifstream input{path};
+
+		while (input >> row) {
+			rows.push_back(row);
+		}
+
+		return rows;
+	}
+
+	// Get the path to the input file for @year and @day as a vector of strings
+	std::string get_data_path(int year, int day) {
+		auto current{std::filesystem::current_path()};
+		auto year_folder{std::to_string(year)};
+		std::ostringstream ss;
+		ss << "day" << std::setw(2) << std::setfill('0') << day << ".txt";
+		std::string day_file(ss.str());
+
+		while (!std::filesystem::exists(current / "AoC-data")) {
+			if (!current.has_parent_path()) {
+				throw std::domain_error("Cannot find data.");
+			}
+			current = current.parent_path();
+		}
+
+		current /= "AoC-data";
+		current /= year_folder;
+		current /= day_file;
+
+		return current.string();
+	}
+}
