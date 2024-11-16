01a3a5f4531ff03ceffd98e749aa2098cd082c89
Peter van der Heijden <heijp06@gmail.com>
2024-11-16 13:50:23 +0100

    Read input from file.

--- 2018/day01/day01_main.cpp
+++ 2018/day01/day01_main.cpp
@@ -1,10 +1,27 @@
+#include <fstream>
 #include <iostream>
+#include <vector>
 
 #include "day01.h"
+#include "day01_main.h"
 
 int main() {
-	std::cout << "Part 1: " << day01::part1({}) << std::endl;
-	std::cout << "Part 2: " << day01::part2({}) << std::endl;
+	auto rows = get_rows("C:/git/AoC-Data/2018/day01.txt");
+
+	std::cout << "Part 1: " << day01::part1(rows) << std::endl;
+	std::cout << "Part 2: " << day01::part2(rows) << std::endl;
 
 	return 0;
+}
+
+std::vector<std::string> get_rows(const std::string& path) {
+	std::vector<std::string> rows{};
+	std::string row;
+	std::ifstream input{path};
+
+	while (input >> row) {
+		rows.push_back(row);
+	}
+
+	return rows;
 }
