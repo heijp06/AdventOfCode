0a797e0688970c0db7792f2b4af548ab74b2d91d
Peter van der Heijden <heijp06@gmail.com>
2024-11-19 20:18:20 +0100

    Implement ints.

--- 2018/lib/advent.h
+++ 2018/lib/advent.h
@@ -2,6 +2,7 @@
 
 #include <filesystem>
 #include <fstream>
+#include <regex>
 #include <sstream>
 #include <stdexcept>
 #include <string>
@@ -54,6 +55,17 @@ namespace advent {
 
 	std::vector<int> ints(std::string row) {
 		std::vector<int> result{};
+		std::regex int_regex{R"([+-]?\d+)"};
+		auto& begin = std::sregex_iterator(row.cbegin(), row.cend(), int_regex);
+		const auto& end = std::sregex_iterator();
+
+		for (auto& it = begin; it != end; it++) {
+			int value;
+			const auto& match = *it;
+            std::istringstream rs{match.str()};
+            rs >> value;
+			result.push_back(value);
+		}
 
 		return result;
 	}
--- 2018/lib/advent_test.cpp
+++ 2018/lib/advent_test.cpp
@@ -11,7 +11,13 @@ struct test_data_ints {
 
 TEST_CASE("changes") {
     const auto& item = GENERATE(
-        test_data_ints{"", {}}
+        test_data_ints{"", {}},
+        test_data_ints{"1", {1}},
+        test_data_ints{"+1", {1}},
+        test_data_ints{"0", {0}},
+        test_data_ints{"-1", {-1}},
+        test_data_ints{"-1,0,1", {-1, 0, 1}},
+        test_data_ints{"asd12asd+12,,-4,assd--ad-+f+5  0 1 2", {12, 12, -4, 5, 0, 1, 2}}
     );
 
     const std::vector<int>& actual = advent::ints(item.row);
