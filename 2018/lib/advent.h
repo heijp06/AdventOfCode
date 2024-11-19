67ce46a8ec5cb3b9cade1a7bf75f30f8b3464d71
Peter van der Heijden <heijp06@gmail.com>
2024-11-19 19:35:53 +0100

    Add CMakeLists.txt and advent_test.cpp to test and build advent.h.

--- 2018/lib/advent.h
+++ 2018/lib/advent.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <filesystem>
+#include <fstream>
 #include <sstream>
 #include <stdexcept>
 #include <string>
---
+++ 2018/lib/advent_test.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+#include <string>
+
+#include "catch.hpp"
+#include "advent.h"
+
+TEST_CASE("changes") {
+    REQUIRE(true);
+}
