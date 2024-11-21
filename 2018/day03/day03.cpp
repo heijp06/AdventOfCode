49e4de06263a8ccf850ed440261b5f85896aee67
Peter van der Heijden <heijp06@gmail.com>
2024-11-21 21:16:09 +0100

    Make claim a simple struct.

--- 2018/day03/day03.cpp
+++ 2018/day03/day03.cpp
@@ -19,30 +19,6 @@ namespace day03 {
 
     claim parse(std::string row) {
         const auto& ints = advent::ints(row);
-        return claim(ints[0], ints[1], ints[2], ints[3], ints[4]);
-    }
-
-    claim::claim(int id, int left, int top, int width, int height) :
-        id_{id}, left_{left}, top_{top}, width_{width}, height_{height} {
-    }
-
-    int claim::id() const {
-        return id_;
-    }
-
-    int claim::left() const {
-        return left_;
-    }
-
-    int claim::top() const {
-        return top_;
-    }
-
-    int claim::width() const {
-        return width_;
-    }
-
-    int claim::height() const {
-        return height_;
+        return claim{ints[0], ints[1], ints[2], ints[3], ints[4]};
     }
 }
--- 2018/day03/day03.h
+++ 2018/day03/day03.h
@@ -4,20 +4,12 @@
 #include <vector>
 
 namespace day03 {
-    class claim {
-    public:
-        claim(int id, int left, int top, int width, int height);
-        int id() const;
-        int left() const;
-        int top() const;
-        int width() const;
-        int height() const;
-    private:
-        int id_;
-        int left_;
-        int top_;
-        int width_;
-        int height_;
+    struct claim {
+        int id;
+        int left;
+        int top;
+        int width;
+        int height;
     };
 
     int part1(const std::vector<std::string>& rows);
