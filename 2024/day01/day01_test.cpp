753b2b2db126ee04a7d1b9b8ebb08697ef56d63d
Peter van der Heijden <heijp06@gmail.com>
2024-12-01 08:16:01 +0100

    Add failing test.

--- 2024/day01/day01_test.cpp
+++ 2024/day01/day01_test.cpp
@@ -1,6 +1,18 @@
 #include "../../lib/catch.hpp"
 #include "day01.h"
 
+#include <vector>
+#include <string>
+
 TEST_CASE("part1") {
-    REQUIRE(true);
+    std::vector<std::string> rows = {
+		"3   4",
+		"4   3",
+		"2   5",
+		"1   3",
+		"3   9",
+		"3   3"
+    };
+
+    REQUIRE(day01::part1(rows) == 11);
 }
