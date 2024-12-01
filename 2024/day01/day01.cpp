2664849972cc7b92c8ec9334ac0fae608e46004a
Peter van der Heijden <heijp06@gmail.com>
2024-12-01 08:39:12 +0100

    Test passes.

--- 2024/day01/day01.cpp
+++ 2024/day01/day01.cpp
@@ -1,13 +1,40 @@
+#include <algorithm>
+#include <cmath>
+#include <iterator>
+
 #include "day01.h"
+#include "../../lib/advent.h"
 
 namespace day01 {
     int part1(const std::vector<std::string>& rows) {
-        (void)rows;
-        return -1;
+        auto sum{0};
+        std::vector<std::vector<int>> pairs;
+
+        std::transform(rows.cbegin(), rows.cend(), std::back_inserter(pairs), advent::ints);
+
+        const auto& list0 = get_list(0, pairs);
+        const auto& list1 = get_list(1, pairs);
+
+        for (size_t i = 0; i < list1.size(); i++) {
+			sum += std::abs(list0[i] - list1[i]);
+        }
+
+        return sum;
     }
 
     int part2(const std::vector<std::string>& rows) {
         (void)rows;
         return -1;
+    }
+    std::vector<int> get_list(int index, const std::vector<std::vector<int>> ids) {
+        std::vector<int> list;
+
+        for (const auto& pair : ids) {
+            list.push_back(pair.at(index));
+        }
+
+        std::sort(list.begin(), list.end());
+
+        return list;
     }
 }
--- 2024/day01/day01.h
+++ 2024/day01/day01.h
@@ -4,4 +4,6 @@
 namespace day01 {
     int part1(const std::vector<std::string>& rows);
     int part2(const std::vector<std::string>& rows);
+
+    std::vector<int> get_list(int index, const std::vector<std::vector<int>> ids);
 }
