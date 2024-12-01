7517e741c5a683aa126418c8b935e6b87867cea5
Peter van der Heijden <peter@vanderheijden.name>
2024-12-01 09:32:50 +0100

    Set default year to 2024 and add option to override.

--- lib/advent.sh
+++ lib/advent.sh
@@ -1,8 +1,8 @@
 set -e
 
-if [[ $# -ne 1 ]]
+if [[ $# -ne 1 && $# -ne 2 ]]
 then
-    echo "Usage: $0 <day number>"
+    echo "Usage: $0 <day number> [year]"
     exit 1
 fi
 
@@ -12,10 +12,16 @@ N=$1
 NN=$(printf "%02d" $1)
 DAY=day$NN
 
+YEAR=2024
+if [[ $# -eq 2 ]]
+then
+    YEAR=$2
+fi
+
 mkdir "${DAY}"
 
 cp ${MYDIR}/dayNN/CMakeLists.txt "${DAY}/CMakeLists.txt"
 sed -e "s/__NN__/$NN/g" ${MYDIR}/dayNN/dayNN.cpp > ${DAY}/${DAY}.cpp
 sed -e "s/__NN__/$NN/g" ${MYDIR}/dayNN/dayNN.h > ${DAY}/${DAY}.h
-sed -e "s/__NN__/$NN/g" -e "s/__N__/$N/g" ${MYDIR}/dayNN/dayNN_main.cpp > ${DAY}/${DAY}_main.cpp
+sed -e "s/__NN__/$NN/g" -e "s/__N__/$N/g" -e "s/__YEAR__/$YEAR/g" ${MYDIR}/dayNN/dayNN_main.cpp > ${DAY}/${DAY}_main.cpp
 sed -e "s/__NN__/$NN/g" ${MYDIR}/dayNN/dayNN_test.cpp > ${DAY}/${DAY}_test.cpp
