b9a17dfabec5619ac91bc9aaa5d49aa0923fa655
Peter van der Heijden <heijp06@gmail.com>
2024-12-02 06:27:44 +0100

    Add test for part 1.

--- 2024/day02/day02_test.cpp
+++ 2024/day02/day02_test.cpp
@@ -1,6 +1,18 @@
+#include <string>
+#include <vector>
+
 #include "../../lib/catch.hpp"
 #include "day02.h"
 
+std::vector<std::string> rows = {
+    "7 6 4 2 1",
+    "1 2 7 8 9",
+    "9 7 6 2 1",
+    "1 3 2 4 5",
+    "8 6 4 4 1",
+    "1 3 6 7 9"
+};
+
 TEST_CASE("part1") {
-    REQUIRE(true);
+    REQUIRE(day02::part1(rows) == 2);
 }
