8863dc0d9ce066f5019359d1ff63e8dac1676d7d
Peter van der Heijden <peter.vd.heijden@malvernpanalytical.com>
2024-12-02 17:10:14 +0100

    Make the row argument to inst const&.

--- lib/advent.h
+++ lib/advent.h
@@ -6,6 +6,6 @@
 namespace advent {
 	std::vector<std::string> get_rows(int year, int day);
 	std::string get_data_path(int year, int day);
-	std::vector<int> ints(std::string row);
+	std::vector<int> ints(const std::string& row);
 }
 
