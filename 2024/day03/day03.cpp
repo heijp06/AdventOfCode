47c9307d38f79dc78c089ead228cc74a0611f3cb
Peter van der Heijden <heijp06@gmail.com>
2024-12-03 19:38:21 +0100

    Add parentheses after do and don't functions.

--- 2024/day03/day03.cpp
+++ 2024/day03/day03.cpp
@@ -30,7 +30,7 @@ namespace day03 {
 
     int mul(const std::string& row, bool part1) {
         auto result{0};
-		std::regex int_regex{R"---(do(n't)?|mul\(\d{1,3},\d{1,3}\))---"};
+		std::regex int_regex{R"---(do(n't)?\(\)|mul\(\d{1,3},\d{1,3}\))---"};
 		const auto& begin = std::sregex_iterator(row.cbegin(), row.cend(), int_regex);
 		const auto& end = std::sregex_iterator();
 		auto add{true};
@@ -38,12 +38,12 @@ namespace day03 {
 		for (auto it = begin; it != end; ++it) {
 			const auto& match = *it;
 
-			if (match.str() == "do") {
+			if (match.str() == "do()") {
 				add = true;
 				continue;
 			}
 
-			if (match.str() == "don't") {
+			if (match.str() == "don't()") {
 				add = false;
 				continue;
 			}
