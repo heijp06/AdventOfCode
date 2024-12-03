b7c797b10920b4e252a498135272219a068b485d
Peter van der Heijden <heijp06@gmail.com>
2024-12-03 06:31:03 +0100

    Add test for part 1.

--- 2024/day03/day03_test.cpp
+++ 2024/day03/day03_test.cpp
@@ -1,6 +1,13 @@
+#include <string>
+#include <vector>
+
 #include "../../lib/catch.hpp"
 #include "day03.h"
 
+std::vector<std::string> rows = {
+    "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+};
+
 TEST_CASE("part1") {
-    REQUIRE(true);
+    REQUIRE(day03::part1(rows) == 161);
 }
