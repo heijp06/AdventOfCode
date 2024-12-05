66a2e7bf93db5aa392ba735bef6282e92a9db031
Peter van der Heijden <heijp06@gmail.com>
2024-12-05 18:40:55 +0100

    Refactor out parse method.

--- 2024/day05/day05.cpp
+++ 2024/day05/day05.cpp
@@ -1,6 +1,3 @@
-#include <set>
-#include <utility>
-
 #include "day05.h"
 #include "../../lib/advent.h"
 
@@ -8,22 +5,8 @@ namespace day05 {
     int part1(const std::vector<std::string>& rows) {
         std::set<std::pair<int, int>> rules;
         std::vector<std::vector<int>> updates;
-        auto parse_rules{true};
 
-        for (const auto& row: rows) {
-	        if (row.empty()) {
-                parse_rules = false;
-                continue;
-	        }
-
-            if (parse_rules) {
-                const auto& numbers = advent::ints(row);
-                rules.insert(std::make_pair(numbers[0], numbers[1]));
-                continue;
-            }
-
-            updates.push_back(advent::ints(row));
-        }
+        parse(rows, rules, updates);
 
         auto result{0};
 		for (const auto& update: updates) {
@@ -46,5 +29,23 @@ namespace day05 {
     int part2(const std::vector<std::string>& rows) {
         (void)rows;
         return -1;
+    }
+    void parse(const std::vector<std::string>& rows, std::set<std::pair<int, int>>& rules, std::vector<std::vector<int>>& updates) {
+        auto parse_rules{true};
+
+        for (const auto& row: rows) {
+	        if (row.empty()) {
+                parse_rules = false;
+                continue;
+	        }
+
+            if (parse_rules) {
+                const auto& numbers = advent::ints(row);
+                rules.insert(std::make_pair(numbers[0], numbers[1]));
+                continue;
+            }
+
+            updates.push_back(advent::ints(row));
+        }
     }
 }
--- 2024/day05/day05.h
+++ 2024/day05/day05.h
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <set>
 #include <string>
+#include <utility>
 #include <vector>
 
 namespace day05 {
     int part1(const std::vector<std::string>& rows);
     int part2(const std::vector<std::string>& rows);
+
+    void parse(const std::vector<std::string>& rows, std::set<std::pair<int, int>>& rules, std::vector<std::vector<int>>& updates);
 }
