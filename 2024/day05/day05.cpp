f121bd22624d8a906d243694f7ab80ce43ad37e8
Peter van der Heijden <peter.vd.heijden@malvernpanalytical.com>
2024-12-05 07:35:08 +0100

    Check in need to finish.

--- 2024/day05/day05.cpp
+++ 2024/day05/day05.cpp
@@ -1,8 +1,43 @@
+#include <set>
+#include <utility>
+
 #include "day05.h"
+#include "../../lib/advent.h"
 
 namespace day05 {
     int part1(const std::vector<std::string>& rows) {
-        (void)rows;
+        std::set<std::pair<int, int>> rules;
+        std::vector<std::vector<int>> updates;
+        auto parse_rules{true};
+
+        for (const auto& row: rows) {
+	        if (row.empty()) {
+                parse_rules = false;
+                continue;
+	        }
+
+            if (parse_rules) {
+                const auto& numbers = advent::ints(row);
+                rules.insert(std::make_pair(numbers[0], numbers[1]));
+                continue;
+            }
+
+            updates.push_back(advent::ints(row));
+        }
+
+        auto result{0};
+		for (const auto& update: updates) {
+            auto correct{true};
+            for (size_t i = 0; i < update.size() - 1; i++) {
+                const auto& pair = std::make_pair(update[i + 1], update[i]);
+                if (rules.count(pair)) {
+                    correct = false;
+                    break;
+                }
+            }
+            // TODO
+		}
+
         return -1;
     }
 
