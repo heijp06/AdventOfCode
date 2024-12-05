dd71ddc6931780a0a5ade95b02cba3ee298407ad
Peter van der Heijden <peter@vanderheijden.name>
2024-12-05 20:07:17 +0100

    Already include vector and string in test and call dayNN::part1(rows) in test.

--- lib/dayNN/dayNN_test.cpp
+++ lib/dayNN/dayNN_test.cpp
@@ -1,6 +1,11 @@
 #include "../../lib/catch.hpp"
 #include "day__NN__.h"
 
+#include <string>
+#include <vector>
+
+std::vector<std::string> rows;
+
 TEST_CASE("part1") {
-    REQUIRE(true);
+    REQUIRE(day__NN__::part1(rows) == -1);
 }
