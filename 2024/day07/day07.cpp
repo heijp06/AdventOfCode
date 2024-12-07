e5431c8d718024b51e93bcc887fb61037db1e56b
Peter van der Heijden <heijp06@gmail.com>
2024-12-07 08:10:02 +0100

    Use vector instead of set.

--- 2024/day07/day07.cpp
+++ 2024/day07/day07.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cmath>
 #include <set>
 
@@ -33,31 +34,31 @@ namespace day07 {
 
     bool solve(const std::vector<int64_t>& equation, bool part2) {
         int64_t sum = equation[0];
-        auto sums = std::set<int64_t>{0};
+        auto sums = std::vector<int64_t>{0};
 
         for (int i = 1; i < static_cast<int>(equation.size()); i++) {
             const int64_t value = equation[i];
-            std::set<int64_t> new_sums;
+            std::vector<int64_t> new_sums;
             for (const auto item : sums) {
                 auto result = item + value;
                 if (result <= sum) {
-                    new_sums.insert(result);
+                    new_sums.push_back(result);
                 }
                 result = item * value;
                 if (result <= sum) {
-                    new_sums.insert(result);
+                    new_sums.push_back(result);
                 }
                 if (part2) {
                     result = item == 0 ? value : concatenate(item, value);
                     if (result <= sum) {
-                        new_sums.insert(result);
+                        new_sums.push_back(result);
                     }
                 }
             }
             std::swap(sums, new_sums);
         }
 
-        return sums.count(sum);
+        return std::find(sums.cbegin(), sums.cend(), sum) != sums.cend();
     }
 
     int64_t concatenate(const int64_t left, const int64_t right) {
