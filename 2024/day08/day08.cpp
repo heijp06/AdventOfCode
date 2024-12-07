c336d7aa0e3fd186c36aff45b87cf25f815c2894
Peter van der Heijden <heijp06@gmail.com>
2024-12-07 22:29:42 +0100

    Prepare day 8.

---
+++ 2024/day08/day08.cpp
@@ -0,0 +1,13 @@
+#include "day08.h"
+
+namespace day08 {
+    int part1(const std::vector<std::string>& rows) {
+        (void)rows;
+        return -1;
+    }
+
+    int part2(const std::vector<std::string>& rows) {
+        (void)rows;
+        return -1;
+    }
+}
