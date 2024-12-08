dfb8539735b6ebcca48826fc9fc8fa708bb79e5f
Peter van der Heijden <heijp06@gmail.com>
2024-12-08 07:19:48 +0100

    Parse grid.

--- 2024/day08/day08.cpp
+++ 2024/day08/day08.cpp
@@ -2,12 +2,48 @@
 
 namespace day08 {
     int part1(const std::vector<std::string>& rows) {
-        (void)rows;
+        const auto& grid = parse(rows);
+
         return -1;
     }
 
     int part2(const std::vector<std::string>& rows) {
         (void)rows;
         return -1;
+    }
+
+    grid parse(std::vector<std::string> rows) {
+        int height = rows.size();
+        int width = rows[0].size();
+        std::map<char, std::vector<coord>> antennas;
+
+        for (int row = 0; row < height; row++) {
+            const auto& line = rows[row];
+            for (int column = 0; column < width; column++) {
+                auto antenna = line[column];
+                if (antenna != '.') {
+                    antennas[line[column]].push_back({row, column});
+                }
+            }
+        }
+
+        return grid({height, width}, antennas);
+    }
+
+    grid::grid(const coord& size, const antennas_t& antennas) :
+        size_{size},
+        antennas_{antennas} {
+    }
+
+    int grid::get_height() const {
+        return size_.row;
+    }
+
+    int grid::get_width() const {
+        return size_.column;
+    }
+
+    const antennas_t grid::get_antennas() const {
+        return antennas_t();
     }
 }
--- 2024/day08/day08.h
+++ 2024/day08/day08.h
@@ -1,9 +1,30 @@
 #pragma once
 
+#include <map>
 #include <string>
 #include <vector>
 
 namespace day08 {
     int part1(const std::vector<std::string>& rows);
     int part2(const std::vector<std::string>& rows);
+
+    struct coord {
+        int row;
+        int column;
+    };
+
+    using antennas_t = std::map<char, std::vector<coord>>;
+
+    class grid {
+    public:
+        grid(const coord& size, const antennas_t& antennas);
+        int get_height() const;
+        int get_width() const;
+        const antennas_t get_antennas() const;
+    private:
+        const coord& size_;
+        const antennas_t& antennas_;
+    };
+
+    grid parse(std::vector<std::string> rows);
 }
