2ecbd966a36b19e7d56c6922916e440f46f06896
Peter van der Heijden <heijp06@gmail.com>
2024-12-09 06:21:47 +0100

    Add test for part 1.

--- 2024/day09/day09_test.cpp
+++ 2024/day09/day09_test.cpp
@@ -4,8 +4,10 @@
 #include "../../lib/catch.hpp"
 #include "day09.h"
 
-std::vector<std::string> rows;
+std::vector<std::string> rows = {
+    "2333133121414131402"
+};
 
 TEST_CASE("part1") {
-    REQUIRE(day09::part1(rows) == -1);
+    REQUIRE(day09::part1(rows) == 1928);
 }
