0e4baa1d47536e7c33c50628e4d74bda43c8b326
Peter van der Heijden <heijp06@gmail.com>
2024-12-10 21:48:00 +0100

    Create template for day 11.

---
+++ 2024/day11/day11.cpp
@@ -0,0 +1,13 @@
+#include "day11.h"
+
+namespace day11 {
+    int part1(const std::vector<std::string>& rows) {
+        (void)rows;
+        return -1;
+    }
+
+    int part2(const std::vector<std::string>& rows) {
+        (void)rows;
+        return -1;
+    }
+}
