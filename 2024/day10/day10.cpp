932abf842993f55351048d61d4a0001b23247474
Peter van der Heijden <peter.vd.heijden@malvernpanalytical.com>
2024-12-10 07:33:40 +0100

    Solve day 10 part 1.

--- 2024/day10/day10.cpp
+++ 2024/day10/day10.cpp
@@ -1,13 +1,52 @@
+#include <set>
 #include "day10.h"
 
 namespace day10 {
     int part1(const std::vector<std::string>& rows) {
-        (void)rows;
-        return -1;
+        auto result{0};
+        int height = rows.size();
+        int width = rows[0].size();
+
+        for (int row = 0; row < height; row++) {
+            const auto& line = rows[row];
+            for (int column = 0; column < width; column++) {
+				if (line[column] == '0') {
+                    result += score(rows, height, width, { row, column });
+				}
+            }
+        }
+
+
+        return result;
     }
 
     int part2(const std::vector<std::string>& rows) {
         (void)rows;
         return -1;
+    }
+
+    int score(const std::vector<std::string>& rows, int height, int width, const advent::coord& coord)
+    {
+        std::set<advent::coord> positions{coord};
+        std::vector<advent::coord> directions{ {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
+
+        for (int i = 0; i < 9; i++) {
+            std::set<advent::coord> new_positions;
+            char c = '1' + i;
+            for (const auto& position: positions) {
+				for (const auto& direction: directions) {
+                    const auto& new_position = position + direction;
+                    if (new_position.row >= 0 && new_position.row < height &&
+                        new_position.column >= 0 && new_position.column < width &&
+                        rows[new_position.row][new_position.column] == c) {
+                        new_positions.insert(new_position);
+                    }
+				}
+            }
+
+            positions = new_positions;
+        }
+
+        return positions.size();
     }
 }
--- 2024/day10/day10.h
+++ 2024/day10/day10.h
@@ -3,7 +3,11 @@
 #include <string>
 #include <vector>
 
+#include "../../lib/advent.h"
+
 namespace day10 {
     int part1(const std::vector<std::string>& rows);
     int part2(const std::vector<std::string>& rows);
+
+    int score(const std::vector<std::string>& rows, int height, int width, const advent::coord& coord);
 }
