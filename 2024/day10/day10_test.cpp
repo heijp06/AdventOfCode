06065ef1ed0f7b0e9ca7f693af67c60b665e51be
Peter van der Heijden <heijp06@gmail.com>
2024-12-10 06:25:48 +0100

    Write test for part 1.

--- 2024/day10/day10_test.cpp
+++ 2024/day10/day10_test.cpp
@@ -4,8 +4,17 @@
 #include "../../lib/catch.hpp"
 #include "day10.h"
 
-std::vector<std::string> rows;
+std::vector<std::string> rows {
+    "89010123",
+    "78121874",
+    "87430965",
+    "96549874",
+    "45678903",
+    "32019012",
+    "01329801",
+    "10456732",
+};
 
 TEST_CASE("part1") {
-    REQUIRE(day10::part1(rows) == -1);
+    REQUIRE(day10::part1(rows) == 36);
 }
