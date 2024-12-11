5faaaa14dbd892d26d9d1fd6d8b1ba56debdec1d
Peter van der Heijden <heijp06@gmail.com>
2024-12-11 06:33:57 +0100

    Write test for day 11 part 1.

--- 2024/day11/day11_test.cpp
+++ 2024/day11/day11_test.cpp
@@ -4,8 +4,8 @@
 #include "../../lib/catch.hpp"
 #include "day11.h"
 
-std::vector<std::string> rows;
+std::vector<std::string> rows{"125 17"};
 
 TEST_CASE("part1") {
-    REQUIRE(day11::part1(rows) == -1);
+    REQUIRE(day11::part1(rows) == 55312);
 }
