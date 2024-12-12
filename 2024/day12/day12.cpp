7c629038bfc4a58640561b26cfd49cba4d396a0a
Peter van der Heijden <heijp06@gmail.com>
2024-12-12 06:30:44 +0100

    Start implementing part 1.

--- 2024/day12/day12.cpp
+++ 2024/day12/day12.cpp
@@ -2,12 +2,35 @@
 
 namespace day12 {
     int part1(const std::vector<std::string>& rows) {
-        (void)rows;
+        int height = rows.size();
+        int width = rows[0].size();
+        std::set<advent::coord> seen;
+        std::vector<std::set<advent::coord>> regions;
+
+        for (int row = 0; row < height; row++) {
+            const auto& line = rows[row];
+            for (int column = 0; column < width; column++) {
+                auto position = advent::coord{row, column};
+                if (seen.count(position)) {
+                    continue;
+                }
+
+                //TODO: maybe create set based on aray.
+                const auto& region = create_region(rows, position);
+                regions.push_back(region);
+                seen.insert(region.cbegin(), region.cend());
+            }
+        }
+
         return -1;
     }
 
     int part2(const std::vector<std::string>& rows) {
         (void)rows;
         return -1;
+    }
+
+    std::set<advent::coord> create_region(std::vector<std::string> rows, advent::coord position) {
+        return std::set<advent::coord>();
     }
 }
--- 2024/day12/day12.h
+++ 2024/day12/day12.h
@@ -1,9 +1,14 @@
 #pragma once
 
+#include <set>
 #include <string>
 #include <vector>
+
+#include "../../lib/advent.h"
 
 namespace day12 {
     int part1(const std::vector<std::string>& rows);
     int part2(const std::vector<std::string>& rows);
+
+    std::set<advent::coord> create_region(std::vector<std::string> rows, advent::coord position);
 }
