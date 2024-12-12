76cc7e23a80e3c4d3909a9111d420e1863d5eba0
Peter van der Heijden <heijp06@gmail.com>
2024-12-12 06:11:15 +0100

    Add failing test for day 12 part 1.

--- 2024/day12/day12_test.cpp
+++ 2024/day12/day12_test.cpp
@@ -4,8 +4,19 @@
 #include "../../lib/catch.hpp"
 #include "day12.h"
 
-std::vector<std::string> rows;
+std::vector<std::string> rows{
+    "RRRRIICCFF",
+    "RRRRIICCCF",
+    "VVRRRCCFFF",
+    "VVRCCCJFFF",
+    "VVVVCJJCFE",
+    "VVIVCCJJEE",
+    "VVIIICJJEE",
+    "MIIIIIJJEE",
+    "MIIISIJEEE",
+    "MMMISSJEEE"
+};
 
 TEST_CASE("part1") {
-    REQUIRE(day12::part1(rows) == -1);
+    REQUIRE(day12::part1(rows) == 1930);
 }
