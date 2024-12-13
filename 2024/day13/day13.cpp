df7c3489c1865db368e9dd9b191d3c4ff6dfebb5
Peter van der Heijden <heijp06@gmail.com>
2024-12-13 20:40:34 +0100

    Refactor code for day 13.

--- 2024/day13/day13.cpp
+++ 2024/day13/day13.cpp
@@ -3,45 +3,34 @@
 #include "day13.h"
 
 namespace day13 {
-    int part1(const std::vector<std::string>& rows) {
-        auto tokens{0};
-
-        for (size_t i = 0; i < rows.size(); i += 4) {
-            auto data = advent::ints(rows[i]);
-            advent::coord a = { data[1], data[0] };
-
-            data = advent::ints(rows[i + 1]);
-            advent::coord b = { data[1], data[0] };
-
-            data = advent::ints(rows[i + 2]);
-            advent::coord prize = { data[1], data[0] };
-
-            tokens += solve(a, b, prize);
-        }
-
-        return tokens;
+    int64_t part1(const std::vector<std::string>& rows) {
+        return solve(rows);
     }
 
     int64_t part2(const std::vector<std::string>& rows) {
+        return solve(rows, true);
+    }
+
+    int64_t solve(const std::vector<std::string>& rows, bool part2) {
         int64_t tokens{0};
 
         for (size_t i = 0; i < rows.size(); i += 4) {
             auto data = advent::ints(rows[i]);
-            advent::coord a = { data[1], data[0] };
+            advent::coord a = {data[1], data[0]};
 
             data = advent::ints(rows[i + 1]);
-            advent::coord b = { data[1], data[0] };
+            advent::coord b = {data[1], data[0]};
 
             data = advent::ints(rows[i + 2]);
-            advent::coord prize = { data[1], data[0] };
+            advent::coord prize = {data[1], data[0]};
 
-            tokens += solve(a, b, prize, true);
+            tokens += solve_equations(a, b, prize, part2);
         }
 
         return tokens;
     }
 
-    int64_t solve(const advent::coord& a, const advent::coord& b, const advent::coord& prize, bool part2) {
+    int64_t solve_equations(const advent::coord& a, const advent::coord& b, const advent::coord& prize, bool part2) {
         int64_t to_add = part2 ? 10'000'000'000'000 : 0;
         int64_t factor = a.row * b.column - a.column * b.row;
         int64_t sum = a.row * (prize.column + to_add) - a.column * (prize.row + to_add);
--- 2024/day13/day13.h
+++ 2024/day13/day13.h
@@ -6,8 +6,9 @@
 #include "../../lib/advent.h"
 
 namespace day13 {
-    int part1(const std::vector<std::string>& rows);
+    int64_t part1(const std::vector<std::string>& rows);
     int64_t part2(const std::vector<std::string>& rows);
 
-    int64_t solve(const advent::coord& a, const advent::coord& b, const advent::coord& prize, bool part2 = false);
+    int64_t solve(const std::vector<std::string>& rows, bool part2 = false);
+    int64_t solve_equations(const advent::coord& a, const advent::coord& b, const advent::coord& prize, bool part2);
 }
