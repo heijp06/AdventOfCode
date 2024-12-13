9974ac7875ed4ad649ef8ca4a55c236519a36268
Peter van der Heijden <peter.vd.heijden@malvernpanalytical.com>
2024-12-13 07:30:54 +0100

    Solve day 13 part 1.

--- 2024/day13/day13.cpp
+++ 2024/day13/day13.cpp
@@ -1,9 +1,37 @@
 #include "day13.h"
+#include "../../lib/advent.h"
 
 namespace day13 {
     int part1(const std::vector<std::string>& rows) {
-        (void)rows;
-        return -1;
+        auto tokens{0};
+
+        for (size_t i = 0; i < rows.size(); i += 4) {
+            auto data = advent::ints(rows[i]);
+            advent::coord a = { data[1], data[0] };
+
+            data = advent::ints(rows[i + 1]);
+            advent::coord b = { data[1], data[0] };
+
+            data = advent::ints(rows[i + 2]);
+            advent::coord prize = { data[1], data[0] };
+
+            auto min_cost{ 0 };
+            for (int i = 0; i <= 100; i++) {
+                for (int j = 0; j <= 100; j++) {
+                    if (i * a.row + j * b.row == prize.row &&
+                        i * a.column + j * b.column == prize.column) {
+                        auto cost = 3 * i + j;
+                        if (!min_cost || cost < min_cost) {
+                            min_cost = cost;
+                        }
+                    }
+                }
+            }
+
+            tokens += min_cost;
+        }
+
+        return tokens;
     }
 
     int part2(const std::vector<std::string>& rows) {
