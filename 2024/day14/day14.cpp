c3c7f05541ee4a74de7b08ee262644d3510af7d9
Peter van der Heijden <heijp06@gmail.com>
2024-12-14 06:22:02 +0100

    Create day 14 template.

---
+++ 2024/day14/day14.cpp
@@ -0,0 +1,13 @@
+#include "day14.h"
+
+namespace day14 {
+    int part1(const std::vector<std::string>& rows) {
+        (void)rows;
+        return -1;
+    }
+
+    int part2(const std::vector<std::string>& rows) {
+        (void)rows;
+        return -1;
+    }
+}
---
+++ 2024/day14/day14_main.cpp
@@ -0,0 +1,15 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#include "../../lib/advent.h"
+#include "day14.h"
+
+int main() {
+	const auto& rows = advent::get_rows(2024, 14);
+
+	std::cout << "Part 1: " << day14::part1(rows) << std::endl;
+	std::cout << "Part 2: " << day14::part2(rows) << std::endl;
+
+	return 0;
+}
