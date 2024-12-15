19530765d9950ff5d5e844f17671778539649bfd
Peter van der Heijden <heijp06@gmail.com>
2024-12-15 06:53:53 +0100

    Add test for part 1.

--- 2024/day15/day15_test.cpp
+++ 2024/day15/day15_test.cpp
@@ -4,8 +4,19 @@
 #include "../../lib/catch.hpp"
 #include "day15.h"
 
-std::vector<std::string> rows;
+std::vector<std::string> rows {
+    "########",
+    "#..O.O.#",
+    "##@.O..#",
+    "#...O..#",
+    "#.#.O..#",
+    "#...O..#",
+    "#......#",
+    "########",
+    "",
+    "<^^ >> > vv<v >> v <<"
+};
 
 TEST_CASE("part1") {
-    REQUIRE(day15::part1(rows) == -1);
+    REQUIRE(day15::part1(rows) == 2028);
 }
