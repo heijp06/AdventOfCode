04684f9d80096206ddacf9145b3a03764f225437
Peter van der Heijden <heijp06@gmail.com>
2024-12-19 19:44:54 +0100

    Factor out a create_design method.

--- 2024/day19/day19.cpp
+++ 2024/day19/day19.cpp
@@ -14,37 +14,7 @@ namespace day19 {
         int result = 0;
 
         for (const auto& design : designs) {
-            auto size = static_cast<int>(design.size());
-            auto indices = std::vector<int>({-1});
-            std::set<int> seen;
-            auto found{false};
-            while (!indices.empty() && !found) {
-                std::vector<int> new_indices;
-                for (const int index : indices) {
-                    int towel_code = 0;
-                    for (int offset = 1; offset <= 8; offset++) {
-                        auto new_index = index + offset;
-                        if (new_index >= size || found) {
-                            break;
-                        }
-                        towel_code = 6 * towel_code + get_color_code(design[new_index]);
-                        if (towel_codes[towel_code]) {
-                            if (new_index == size - 1) {
-                                found = true;
-                            }
-                            else if (!seen.count(new_index)) {
-                                seen.insert(new_index);
-                                new_indices.push_back(new_index);
-                            }
-                        }
-                    }
-                    if (found) {
-                        break;
-                    }
-                }
-                indices = new_indices;
-            }
-            result += found;
+            result += create_design(design, towel_codes);
         }
 
         return result;
@@ -53,6 +23,40 @@ namespace day19 {
     int part2(const std::vector<std::string>& rows) {
         (void)rows;
         return -1;
+    }
+
+    bool day19::create_design(const std::string& design, const std::vector<bool>& towel_codes) {
+        auto size = static_cast<int>(design.size());
+        auto indices = std::vector<int>({-1});
+        std::set<int> seen;
+        auto found{false};
+        while (!indices.empty() && !found) {
+            std::vector<int> new_indices;
+            for (const int index : indices) {
+                int towel_code = 0;
+                for (int offset = 1; offset <= 8; offset++) {
+                    auto new_index = index + offset;
+                    if (new_index >= size || found) {
+                        break;
+                    }
+                    towel_code = 6 * towel_code + get_color_code(design[new_index]);
+                    if (towel_codes[towel_code]) {
+                        if (new_index == size - 1) {
+                            found = true;
+                        }
+                        else if (!seen.count(new_index)) {
+                            seen.insert(new_index);
+                            new_indices.push_back(new_index);
+                        }
+                    }
+                }
+                if (found) {
+                    break;
+                }
+            }
+            indices = new_indices;
+        }
+        return found;
     }
 
     std::vector<bool> get_towel_codes(const std::vector<std::string>& towels) {
--- 2024/day19/day19.h
+++ 2024/day19/day19.h
@@ -7,6 +7,7 @@ namespace day19 {
     int part1(const std::vector<std::string>& rows);
     int part2(const std::vector<std::string>& rows);
 
+    bool create_design(const std::string& design, const std::vector<bool>& towel_codes);
     std::vector<bool> get_towel_codes(const std::vector<std::string>& towels);
     int get_color_code(const char c);
 }
