d4bb8ddbaf96f90e086114ff0871da1748cf3705
Peter van der Heijden <heijp06@gmail.com>
2024-12-19 15:41:26 +0100

    Build usage table of color_codes.

--- 2024/day19/day19.cpp
+++ 2024/day19/day19.cpp
@@ -2,9 +2,22 @@
 #include "../../lib/advent.h"
 
 namespace day19 {
+    constexpr int six_pow_eight = 36 * 36 * 36 * 36;
+
     int part1(const std::vector<std::string>& rows) {
         const auto& towels = advent::split(rows[0], ", ");
         const auto& designs = std::vector<std::string>(rows.cbegin() + 2, rows.cend());
+        const std::string& color_codes = "wubrg";
+
+        auto table = std::vector<bool>(six_pow_eight);
+        for (const auto& towel : towels) {
+            int towel_code = 0;
+            for (char c : towel) {
+                int color_code = static_cast<int>(color_codes.find(c)) + 1;
+                towel_code = towel_code * 6 + color_code;
+            }
+            table[towel_code] = true;
+        }
 
         return -1;
     }
