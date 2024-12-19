b0cf41b84c3a42a9e3ca9244fd52053913c39eba
Peter van der Heijden <heijp06@gmail.com>
2024-12-19 07:22:53 +0100

    Remove equals and not equals for reindeer.

--- 2024/day16/day16.h
+++ 2024/day16/day16.h
@@ -10,13 +10,6 @@ namespace day16 {
         advent::coord position;
         advent::direction direction;
 
-        friend bool operator==(const reindeer& left, const reindeer& right) {
-            return left.position == right.position
-                && left.direction == right.direction;
-        }
-        friend bool operator!=(const reindeer& left, const reindeer& right) {
-            return !(left == right);
-        }
         friend bool operator<(const reindeer& left, const reindeer& right) {
             if (left.position != right.position) {
                 return left.position < right.position;
