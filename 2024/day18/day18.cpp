ffa2119cb62b0233f7daf50562dab3e57294ef08
Peter van der Heijden <heijp06@gmail.com>
2024-12-19 22:30:51 +0100

    Create grid.

--- 2024/day18/day18.cpp
+++ 2024/day18/day18.cpp
@@ -1,8 +1,18 @@
 #include "day18.h"
+#include "../../lib/advent.h"
 
 namespace day18 {
     int part1(const std::vector<std::string>& rows, int size, int nanoseconds) {
-        (void)rows;
+        std::vector<std::vector<int>> grid;
+        for (int row = 0; row <= size; row++) {
+            grid.push_back(std::vector<int>(size + 1));
+        }
+
+        for (int i = 0; i < nanoseconds; i++) {
+            const auto& coords = advent::ints(rows[i]);
+            grid[coords[1]][coords[0]] = i + 1;
+        }
+
         return -1;
     }
 
