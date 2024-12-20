225a940674135a341a4500c44daa28b280efb9a6
Peter van der Heijden <heijp06@gmail.com>
2024-12-20 21:52:51 +0100

    Add test for part 2.

--- 2018/day03/day03_test.cpp
+++ 2018/day03/day03_test.cpp
@@ -1,12 +1,16 @@
 #include "../../lib/catch.hpp"
 #include "day03.h"
 
-TEST_CASE("part1") {
-    std::vector<std::string> rows = {
-        "#1 @ 1,3: 4x4",
-        "#2 @ 3,1: 4x4",
-        "#3 @ 5,5: 2x2"
-    };
+std::vector<std::string> rows = {
+    "#1 @ 1,3: 4x4",
+    "#2 @ 3,1: 4x4",
+    "#3 @ 5,5: 2x2"
+};
 
+TEST_CASE("part1") {
     REQUIRE(day03::part1(rows) == 4);
+}
+
+TEST_CASE("part2") {
+    REQUIRE(day03::part2(rows) == 3);
 }
