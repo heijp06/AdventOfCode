f5e9dbe880e6c06b5d2507cd074fe58c993f4137
Peter van der Heijden <heijp06@gmail.com>
2024-12-20 22:02:21 +0100

    Solve 2018 day 3 part 2.

--- 2018/day03/day03.cpp
+++ 2018/day03/day03.cpp
@@ -41,7 +41,46 @@ namespace day03 {
     }
 
     int part2(const std::vector<std::string>& rows) {
-        (void)rows;
+        std::vector<claim> claims;
+
+        auto height{0};
+        auto width{0};
+        for (const auto& row : rows) {
+            const auto& claim = parse(row);
+            height = std::max(height, claim.top + claim.height);
+            width = std::max(width, claim.left + claim.width);
+            claims.push_back(claim);
+        }
+
+        std::vector<std::vector<int>> grid;
+        for (int row = 0; row < height; row++) {
+            grid.push_back(std::vector<int>(width));
+        }
+
+        for (const auto& claim : claims) {
+            for (int row = 0; row < claim.height; row++) {
+                auto& line = grid[claim.top + row];
+                for (int column = 0; column < claim.width; column++) {
+                    line[claim.left + column]++;
+                }
+            }
+        }
+
+        for (const auto& claim : claims) {
+            auto valid{true};
+            for (int row = 0; row < claim.height && valid; row++) {
+                auto& line = grid[claim.top + row];
+                for (int column = 0; column < claim.width && valid; column++) {
+                    if (line[claim.left + column] != 1) {
+                        valid = false;
+                    }
+                }
+            }
+            if (valid) {
+                return claim.id;
+            }
+        }
+
         return -1;
     }
 
