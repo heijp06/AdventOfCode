76498e63483962e2032f6c0c24618841e6c23a5e
Peter van der Heijden <heijp06@gmail.com>
2024-12-21 07:12:32 +0100

    Add test for day 21 part 1.

--- 2024/day21/day21_test.cpp
+++ 2024/day21/day21_test.cpp
@@ -4,8 +4,14 @@
 #include "../../lib/catch.hpp"
 #include "day21.h"
 
-std::vector<std::string> rows;
+std::vector<std::string> rows{
+    "029A",
+    "980A",
+    "179A",
+    "456A",
+    "379A"
+};
 
 TEST_CASE("part1") {
-    REQUIRE(day21::part1(rows) == -1);
+    REQUIRE(day21::part1(rows) == 126384);
 }
