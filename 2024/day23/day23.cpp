40eb019a53fd8121b37a9bd3773a681bb03cd50a
Peter van der Heijden <heijp06@gmail.com>
2024-12-23 09:43:40 +0100

    Write code for part 2.

--- 2024/day23/day23.cpp
+++ 2024/day23/day23.cpp
@@ -22,12 +22,53 @@ namespace day23 {
     }
 
     int part2(const std::vector<std::string>& rows) {
-        (void)rows;
+        const auto& pairs = get_pairs(rows);
+        const auto& lan_parties = get_triples(pairs);
+
+        while (true) {
+            std::vector<std::set<std::string>> new_lan_parties;
+            for (const auto& lan_party : lan_parties) {
+                const auto& first_member = *(lan_party.cbegin());
+                for (const auto& new_member : pairs.at(first_member)) {
+                    // Check if new member is really new
+                    if (lan_party.count(new_member)) {
+                        continue;
+                    }
+
+                    for (auto& it = ++lan_party.cbegin(); it != lan_party.cend(); it++) {
+                        const auto& old_member = *it;
+
+                        // All members of the lan party should be friends with the new member.
+                        if (new_member < old_member) {
+                            const auto& new_friends = pairs.at(new_member);
+                            if (std::find(
+                                new_friends.cbegin(), new_friends.cend(), old_member) == new_friends.cend()) {
+                                continue;
+                            }
+                        }
+
+                        if (old_member < new_member) {
+                            const auto& old_friends = pairs.at(old_member);
+                            if (std::find(
+                                old_friends.cbegin(), old_friends.cend(), new_member) == old_friends.cend()) {
+                                continue;
+                            }
+                        }
+
+                        // A true friend! Add it to the party.
+                        auto new_lan_party = std::set<std::string>({new_member});
+                        new_lan_party.insert(lan_party.cbegin(), lan_party.cend());
+                        new_lan_parties.push_back(new_lan_party);
+                    }
+                }
+            }
+        }
+
         return -1;
     }
 
-    std::vector<std::vector<std::string>> get_triples(std::unordered_map<std::string, std::vector<std::string>> pairs) {
-        std::vector<std::vector<std::string>> triples;
+    std::vector<std::set<std::string>> get_triples(std::unordered_map<std::string, std::vector<std::string>> pairs) {
+        std::vector<std::set<std::string>> triples;
 
         for (const auto& [value1, values1] : pairs) {
             for (const auto& value2 : values1) {
--- 2024/day23/day23.h
+++ 2024/day23/day23.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <set>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -8,6 +9,6 @@ namespace day23 {
     int part1(const std::vector<std::string>& rows);
     int part2(const std::vector<std::string>& rows);
 
-    std::vector<std::vector<std::string>> get_triples(std::unordered_map<std::string, std::vector<std::string>> pairs);
+    std::vector<std::set<std::string>> get_triples(std::unordered_map<std::string, std::vector<std::string>> pairs);
     std::unordered_map<std::string, std::vector<std::string>> get_pairs(const std::vector<std::string>& rows);
 }
