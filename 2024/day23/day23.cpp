06722b92e0ab8b82609d42119ac5d3e4a51f6679
Peter van der Heijden <heijp06@gmail.com>
2024-12-23 08:08:51 +0100

    Solve day 23 part 1.

--- 2024/day23/day23.cpp
+++ 2024/day23/day23.cpp
@@ -1,9 +1,39 @@
+#include <algorithm>
+#include <unordered_map>
+
 #include "day23.h"
+#include "../../lib/advent.h"
 
 namespace day23 {
     int part1(const std::vector<std::string>& rows) {
-        (void)rows;
-        return -1;
+        std::unordered_map<std::string, std::vector<std::string>> pairs;
+
+        for (const auto& row : rows) {
+            const auto& pair = advent::split(row, "-");
+            if (pair[0] < pair[1]) {
+                pairs[pair[0]].push_back(pair[1]);
+            }
+            else {
+                pairs[pair[1]].push_back(pair[0]);
+            }
+        }
+
+        auto counter{0};
+        for (const auto& [value1, values1] : pairs) {
+            for (const auto& value2 : values1) {
+                if (pairs.count(value2)) {
+                    const auto& values2 = pairs[value2];
+                    for (const auto& value3 : values2) {
+                        const auto& it = std::find(values1.cbegin(), values1.cend(), value3);
+                        if (it != values1.cend()) {
+                            counter += value1[0] == 't' || value2[0] == 't' || value3[0] == 't';
+                        }
+                    }
+                }
+            }
+        }
+
+        return counter;
     }
 
     int part2(const std::vector<std::string>& rows) {
--- 2024/day23/day23_test.cpp
+++ 2024/day23/day23_test.cpp
@@ -5,10 +5,10 @@
 #include "day23.h"
 
 std::vector<std::string> rows{
-    "kh - tc", "qp - kh", "de - cg", "ka - co", "yn - aq", "qp - ub", "cg - tb", "vc - aq",
-    "tb - ka", "wh - tc", "yn - cg", "kh - ub", "ta - co", "de - co", "tc - td", "tb - wq",
-    "wh - td", "ta - ka", "td - qp", "aq - cg", "wq - ub", "ub - vc", "de - ta", "wq - aq",
-    "wq - vc", "wh - yn", "ka - de", "kh - ta", "co - tc", "wh - qp", "tb - vc", "td - yn"
+    "kh-tc", "qp-kh", "de-cg", "ka-co", "yn-aq", "qp-ub", "cg-tb", "vc-aq",
+    "tb-ka", "wh-tc", "yn-cg", "kh-ub", "ta-co", "de-co", "tc-td", "tb-wq",
+    "wh-td", "ta-ka", "td-qp", "aq-cg", "wq-ub", "ub-vc", "de-ta", "wq-aq",
+    "wq-vc", "wh-yn", "ka-de", "kh-ta", "co-tc", "wh-qp", "tb-vc", "td-yn"
 };
 
 TEST_CASE("part1") {
