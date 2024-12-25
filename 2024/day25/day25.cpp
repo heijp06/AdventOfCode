2bf12e6849c8d827702a1258e2737f8204bb589c
Peter van der Heijden <heijp06@gmail.com>
2024-12-25 06:46:29 +0100

    Solve all days of 2024!

--- 2024/day25/day25.cpp
+++ 2024/day25/day25.cpp
@@ -2,8 +2,49 @@
 
 namespace day25 {
     int part1(const std::vector<std::string>& rows) {
-        (void)rows;
-        return -1;
+        std::vector<std::vector<int>> keys;
+        std::vector<std::vector<int>> locks;
+
+        for (size_t i = 0; i < rows.size(); i += 8) {
+            if (rows[i][0] == '#') {
+                std::vector<int> key;
+                for (size_t column = 0; column < 5; column++) {
+                    auto count{0};
+                    for (size_t row = 1; row <= 5; row++) {
+                        count += rows[i + row][column] == '#';
+                    }
+                    key.push_back(count);
+                }
+                keys.push_back(key);
+            }
+            else {
+                std::vector<int> lock;
+                for (size_t column = 0; column < 5; column++) {
+                    auto count{0};
+                    for (size_t row = 1; row <= 5; row++) {
+                        count += rows[i + row][column] == '#';
+                    }
+                    lock.push_back(count);
+                }
+                locks.push_back(lock);
+            }
+        }
+
+        auto result{0};
+
+        for (const auto& key : keys) {
+            for (const auto& lock : locks) {
+                auto fits{true};
+                for (size_t i = 0; i < 5 && fits; i++) {
+                    if (key[i] + lock[i] > 5) {
+                        fits = false;
+                    }
+                }
+                result += fits;
+            }
+        }
+
+        return result;
     }
 
     int part2(const std::vector<std::string>& rows) {
