2789a56d56fcaab48de97972c85115d9797ea9f6
Peter van der Heijden <heijp06@gmail.com>
2024-12-25 05:57:52 +0100

    Create template for day 25.

---
+++ 2024/day25/day25.cpp
@@ -0,0 +1,13 @@
+#include "day25.h"
+
+namespace day25 {
+    int part1(const std::vector<std::string>& rows) {
+        (void)rows;
+        return -1;
+    }
+
+    int part2(const std::vector<std::string>& rows) {
+        (void)rows;
+        return -1;
+    }
+}
