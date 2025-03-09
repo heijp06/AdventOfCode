82f3f294f00bd1cf28ab6a33cc719b387e0a1cdf
Peter van der Heijden <heijp06@gmail.com>
2025-03-09 10:24:28 +0100

    Start 2018 day 4.

---
+++ 2018/day04/day04_test.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+#include "../../lib/catch.hpp"
+#include "day04.h"
+
+std::vector<std::string> rows;
+
+TEST_CASE("part1") {
+    REQUIRE(day04::part1(rows) == -1);
+}
