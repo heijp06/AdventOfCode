d75c9dbd2dc4fa107a6d10d83acebae72206c821
Peter van der Heijden <heijp06@gmail.com>
2025-03-10 11:29:17 +0100

    Solve 2018 day 4 part 1.

--- 2018/day05/day05.cpp
+++ 2018/day05/day05.cpp
@@ -1,9 +1,24 @@
+#include <cctype>
+
 #include "day05.h"
 
 namespace day05 {
     int part1(const std::vector<std::string>& rows) {
-        (void)rows;
-        return -1;
+        std::string result;
+        const auto& polymer = rows.at(0);
+
+        for (const auto unit : polymer) {
+            if (!result.empty()) {
+                const auto back = result.back();
+                if (back != unit && std::tolower(back) == std::tolower(unit)) {
+                    result.pop_back();
+                    continue;
+                }
+            }
+            result.push_back(unit);
+        }
+
+        return static_cast<int>(result.size());
     }
 
     int part2(const std::vector<std::string>& rows) {
