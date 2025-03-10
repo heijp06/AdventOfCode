d9afe89a456ad89ec99dec6593de589027ccde8b
Peter van der Heijden <heijp06@gmail.com>
2025-03-10 11:38:54 +0100

    Solve 2018 day 5 part 2.

--- 2018/day05/day05.cpp
+++ 2018/day05/day05.cpp
@@ -1,13 +1,33 @@
+#include <algorithm>
 #include <cctype>
 
 #include "day05.h"
 
 namespace day05 {
     int part1(const std::vector<std::string>& rows) {
-        std::string result;
         const auto& polymer = rows.at(0);
+        return solve(polymer);
+    }
+
+    int part2(const std::vector<std::string>& rows) {
+        const auto& polymer = rows.at(0);
+        auto min_length{static_cast<int>(polymer.size())};
+
+        for (char c = 'a'; c <= 'z'; c++) {
+            min_length = std::min(min_length, solve(polymer, c));
+        }
+
+        return min_length;
+    }
+
+    int solve(const std::string& polymer, const char skip) {
+        std::string result;
 
         for (const auto unit : polymer) {
+            if (unit == skip || std::tolower(unit) == std::tolower(skip)) {
+                continue;
+            }
+
             if (!result.empty()) {
                 const auto back = result.back();
                 if (back != unit && std::tolower(back) == std::tolower(unit)) {
@@ -15,14 +35,10 @@ namespace day05 {
                     continue;
                 }
             }
+
             result.push_back(unit);
         }
 
         return static_cast<int>(result.size());
-    }
-
-    int part2(const std::vector<std::string>& rows) {
-        (void)rows;
-        return -1;
     }
 }
--- 2018/day05/day05.h
+++ 2018/day05/day05.h
@@ -6,4 +6,6 @@
 namespace day05 {
     int part1(const std::vector<std::string>& rows);
     int part2(const std::vector<std::string>& rows);
+
+    int solve(const std::string& polymer, const char skip = ' ');
 }
