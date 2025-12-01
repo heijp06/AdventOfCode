e03617e33b1ed8e9c6bd7ea27b5150d1115b32c3
Peter van der Heijden <peter.vd.heijden@malvernpanalytical.com>
2025-12-01 17:06:28 +0100

    Add test for part 1.

--- 2025/day01/day01_test.cpp
+++ 2025/day01/day01_test.cpp
@@ -4,8 +4,19 @@
 #include "../../lib/catch.hpp"
 #include "day01.h"
 
-std::vector<std::string> rows;
+std::vector<std::string> rows{
+"L68",
+"L30",
+"R48",
+"L5",
+"R60",
+"L55",
+"L1",
+"L99",
+"R14",
+"L82",
+};
 
 TEST_CASE("part1") {
-    REQUIRE(day01::part1(rows) == -1);
+    REQUIRE(day01::part1(rows) == 3);
 }
