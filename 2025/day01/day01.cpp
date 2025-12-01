1f8b85649cd48cb4af78057617d836a0300f1c0d
Peter van der Heijden <peter.vd.heijden@malvernpanalytical.com>
2025-12-01 07:27:38 +0100

    Solve day 1 part 1.

--- 2025/day01/day01.cpp
+++ 2025/day01/day01.cpp
@@ -2,8 +2,20 @@
 
 namespace day01 {
     int part1(const std::vector<std::string>& rows) {
-        (void)rows;
-        return -1;
+        auto result{0};
+        auto sum{50};
+        for (const auto& row : rows) {
+            const auto number = std::stoi(row.substr(1));
+            if (row[0] == 'L') {
+                sum -= number;
+            }
+            else {
+                sum += number;
+            }
+            result += sum % 100 == 0;
+        }
+
+        return result;
     }
 
     int part2(const std::vector<std::string>& rows) {
