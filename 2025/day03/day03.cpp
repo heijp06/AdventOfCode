69d80955e6823dd1abaca3cd89c3a4b0740d07ae
Peter van der Heijden <heijp06@gmail.com>
2025-12-03 07:12:47 +0100

    Use large int.

--- 2025/day03/day03.cpp
+++ 2025/day03/day03.cpp
@@ -2,7 +2,7 @@
 #include "day03.h"
 
 namespace day03 {
-    int part1(const std::vector<std::string>& rows) {
+    std::int64_t part1(const std::vector<std::string>& rows) {
         auto sum{0};
 
         for (const auto& row : rows) {
@@ -12,12 +12,12 @@ namespace day03 {
         return sum;
     }
 
-    int part2(const std::vector<std::string>& rows) {
+    std::int64_t part2(const std::vector<std::string>& rows) {
         (void)rows;
         return -1;
     }
 
-    int joltage(const std::string& row) {
+    std::int64_t joltage(const std::string& row) {
         const auto& d1 = *std::max_element(row.cbegin(), row.cend() - 1);
         const auto& pos = std::find(row.cbegin(), row.cend(), d1);
         const auto& d2 = *std::max_element(pos + 1, row.cend());
--- 2025/day03/day03.h
+++ 2025/day03/day03.h
@@ -1,11 +1,12 @@
 #pragma once
 
+#include <cstdint>
 #include <string>
 #include <vector>
 
 namespace day03 {
-    int part1(const std::vector<std::string>& rows);
-    int part2(const std::vector<std::string>& rows);
+    std::int64_t part1(const std::vector<std::string>& rows);
+    std::int64_t part2(const std::vector<std::string>& rows);
 
-    int joltage(const std::string& row);
+    std::int64_t joltage(const std::string& row);
 }
