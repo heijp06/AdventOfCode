f8ec69c1161e7b58712dc7753938731091968766
Peter van der Heijden <heijp06@gmail.com>
2025-12-03 08:54:00 +0100

    Make day 2 more efficient.

--- 2025/day02/day02.cpp
+++ 2025/day02/day02.cpp
@@ -1,5 +1,7 @@
+#include <numeric>
 #include <cmath>
 #include <regex>
+#include <set>
 
 #include "../../lib/advent.h"
 #include "day02.h"
@@ -10,7 +12,7 @@ namespace day02 {
 
         for (const auto& pair : advent::split(rows[0], ",")) {
             const auto& nums = advent::ints<std::int64_t>(pair);
-            sum += sum_invalid(nums[0], std::abs(nums[1]), "(.*)\\1");
+            sum += sum_invalid(nums[0], -nums[1], false);
         }
 
         return sum;
@@ -21,23 +23,60 @@ namespace day02 {
 
         for (const auto& pair : advent::split(rows[0], ",")) {
             const auto& nums = advent::ints<std::int64_t>(pair);
-            sum += sum_invalid(nums[0], std::abs(nums[1]), "(.*)(\\1)+");
+            sum += sum_invalid(nums[0], -nums[1], true);
         }
 
         return sum;
     }
 
-    std::int64_t sum_invalid(const std::int64_t& num_start, const std::int64_t& num_end, const std::string& regex)
+    std::int64_t sum_invalid(const std::int64_t& num_start, const std::int64_t& num_end, const bool part2)
     {
-        const std::regex txt_regex(regex);
-        std::int64_t sum{0};
+        std::set<std::int64_t> invalid;
 
-        for (auto i = num_start; i <= num_end; i++) {
-            if (std::regex_match(std::to_string(i), txt_regex)) {
-                sum += i;
+        const auto& start = std::to_string(num_start);
+        const auto& end = std::to_string(num_end);
+
+        auto stop = part2 ? end.length() : 2;
+
+        for (int i = 2; i <= stop; i++) {
+            if (start.length() % i && end.length() % i) {
+                continue;
+            }
+
+            int len = start.length() % i ? end.length() / i : start.length() / i;
+            auto power = pow(10, len);
+            auto div = pow(power, i - 1);
+            std::int64_t val = start.length() % i ? power / 10 : num_start / div;
+
+            std::int64_t num = rep(val++, i, power);
+            while (num <= num_end && val <= power) {
+                if (num >= num_start) {
+                    invalid.insert(num);
+                }
+                num = rep(val++, i, power);
             }
         }
 
-        return sum;
+        return std::reduce(invalid.cbegin(), invalid.cend());
+    }
+
+    std::int64_t pow(const std::int64_t& base, int power) {
+        std::int64_t result{1};
+
+        for (int i = 0; i < power; i++) {
+            result *= base;
+        }
+        return result;
+    }
+
+    std::int64_t rep(const std::int64_t& val, int repetition, const std::int64_t& power) {
+        std::int64_t result{0};
+
+        for (int i = 0; i < repetition; i++) {
+            result *= power;
+            result += val;
+        }
+
+        return result;
     }
 }
--- 2025/day02/day02.h
+++ 2025/day02/day02.h
@@ -8,5 +8,7 @@ namespace day02 {
     std::int64_t part1(const std::vector<std::string>& rows);
     std::int64_t part2(const std::vector<std::string>& rows);
 
-    std::int64_t sum_invalid(const std::int64_t& num_start, const std::int64_t& num_end, const std::string& regex);
+    std::int64_t sum_invalid(const std::int64_t& num_start, const std::int64_t& num_end, const bool part2);
+    std::int64_t pow(const std::int64_t& base, int power);
+    std::int64_t rep(const std::int64_t& val, int repetition, const std::int64_t& power);
 }
