5b63e200601fcd6c588ae0bfdd523cebce7807e8
Peter van der Heijden <heijp06@gmail.com>
2025-12-04 06:47:41 +0100

    Solve day 3 part 1.

--- 2025/day04/day04.cpp
+++ 2025/day04/day04.cpp
@@ -1,9 +1,30 @@
 #include "day04.h"
+#include "../../lib/advent.h"
 
 namespace day04 {
     int part1(const std::vector<std::string>& rows) {
-        (void)rows;
-        return -1;
+        auto result{0};
+        const auto& grid = advent::grid{rows};
+        std::vector<advent::coord> directions{
+            {-1, -1}, {-1, 0}, {-1, 1},
+            {0, -1}, {0, 1},
+            {1, -1}, {1, 0}, {1, 1},
+        };
+
+        for (const auto& current : grid.find_all('@')) {
+            auto counter{0};
+            for (const auto& direction : directions) {
+                const auto& pos = current + direction;
+                if (pos.column >= 0 && pos.column < grid.get_width()
+                    && pos.row >= 0 && pos.row < grid.get_height()
+                    && grid[pos] == '@') {
+                    counter++;
+                }
+            }
+            result += counter < 4;
+        }
+        
+        return result;
     }
 
     int part2(const std::vector<std::string>& rows) {
--- 2025/day04/day04_test.cpp
+++ 2025/day04/day04_test.cpp
@@ -4,8 +4,19 @@
 #include "../../lib/catch.hpp"
 #include "day04.h"
 
-std::vector<std::string> rows;
+std::vector<std::string> rows{
+    "..@@.@@@@.",
+    "@@@.@.@.@@",
+    "@@@@@.@.@@",
+    "@.@@@@..@.",
+    "@@.@@@@.@@",
+    ".@@@@@@@.@",
+    ".@.@.@.@@@",
+    "@.@@@.@@@@",
+    ".@@@@@@@@.",
+    "@.@.@@@.@.",
+};
 
 TEST_CASE("part1") {
-    REQUIRE(day04::part1(rows) == -1);
+    REQUIRE(day04::part1(rows) == 13);
 }
