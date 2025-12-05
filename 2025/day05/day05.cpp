86d475ae235a308455385fc4cf44ffaf9dcfec87
Peter van der Heijden <heijp06@gmail.com>
2025-12-05 06:31:31 +0100

    Solve day 5 part 1.

--- 2025/day05/day05.cpp
+++ 2025/day05/day05.cpp
@@ -1,9 +1,40 @@
+#include <cstdint>
+#include <utility>
+
 #include "day05.h"
+#include "../../lib/advent.h"
+
+using range = std::pair<std::int64_t, std::int64_t>;
 
 namespace day05 {
     int part1(const std::vector<std::string>& rows) {
-        (void)rows;
-        return -1;
+        std::vector<range> ranges{};
+        std::vector<std::int64_t> ingredients;
+
+        for (const auto& row : rows) {
+            const auto& fields = advent::ints<int64_t>(row);
+
+            if (fields.size() == 1) {
+                ingredients.push_back(fields[0]);
+            }
+
+            if (fields.size() == 2) {
+                ranges.push_back(std::make_pair(fields[0], -fields[1]));
+            }
+        }
+
+        auto result{0};
+
+        for (const auto& ingredient : ingredients) {
+            for (const auto& range : ranges) {
+                if (range.first <= ingredient && range.second >= ingredient) {
+                    result++;
+                    break;
+                }
+            }
+        }
+
+        return result;
     }
 
     int part2(const std::vector<std::string>& rows) {
--- 2025/day05/day05_test.cpp
+++ 2025/day05/day05_test.cpp
@@ -4,8 +4,20 @@
 #include "../../lib/catch.hpp"
 #include "day05.h"
 
-std::vector<std::string> rows;
+std::vector<std::string> rows{
+    "3-5",
+    "10-14",
+    "16-20",
+    "12-18",
+    "",
+    "1",
+    "5",
+    "8",
+    "11",
+    "17",
+    "32",
+};
 
 TEST_CASE("part1") {
-    REQUIRE(day05::part1(rows) == -1);
+    REQUIRE(day05::part1(rows) == 3);
 }
