89313a23974273db9eabf05d367561a8b19db667
Peter van der Heijden <heijp06@gmail.com>
2025-12-06 11:39:18 +0100

    Add test for part 2.

--- lib/dayNN/dayNN_test.cpp
+++ lib/dayNN/dayNN_test.cpp
@@ -4,8 +4,14 @@
 #include "../../lib/catch.hpp"
 #include "day__NN__.h"
 
-std::vector<std::string> rows;
+std::vector<std::string> rows{
+
+};
 
 TEST_CASE("part1") {
     REQUIRE(day__NN__::part1(rows) == -1);
+}
+
+TEST_CASE("part2") {
+    REQUIRE(day__NN__::part2(rows) == -1);
 }
