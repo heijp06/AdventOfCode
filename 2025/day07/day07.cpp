03f0116e78bc3f8fc882145ce2ab4dccf9310dbe
Peter van der Heijden <heijp06@gmail.com>
2025-12-07 07:01:26 +0100

    Refactor day 7.

--- 2025/day07/day07.cpp
+++ 2025/day07/day07.cpp
@@ -6,41 +6,20 @@
 
 namespace day07 {
     std::int64_t part1(const std::vector<std::string>& rows) {
-        const auto& manifold = advent::grid{rows};
-        auto start = manifold.find('S').column;
-        auto current_row = std::vector<bool>(manifold.get_width());
-        current_row[start] = true;
-        auto next_row = std::vector<bool>(manifold.get_width());
-        int split{};
-
-        for (int row = 1; row < manifold.get_height(); row++) {
-            for (int column = 0; column < manifold.get_width(); column++) {
-                if (!current_row[column]) {
-                    continue;
-                }
-
-                if (manifold[{row, column}] == '^') {
-                    next_row[column - 1] = true;
-                    next_row[column + 1] = true;
-                    split++;
-                }
-                else {
-                    next_row[column] = true;
-                }
-            }
-            std::swap(current_row, next_row);
-            std::fill(next_row.begin(), next_row.end(), false);
-        }
-        
-        return split;
+        return solve(rows, false);
     }
 
     std::int64_t part2(const std::vector<std::string>& rows) {
+        return solve(rows, true);
+    }
+
+    int64_t day07::solve(const std::vector<std::string>& rows, bool part2) {
         const auto& manifold = advent::grid{rows};
         auto start = manifold.find('S').column;
         auto current_row = std::vector<std::int64_t>(manifold.get_width());
         current_row[start] = true;
         auto next_row = std::vector<std::int64_t>(manifold.get_width());
+        std::int64_t split{};
 
         for (int row = 1; row < manifold.get_height(); row++) {
             for (int column = 0; column < manifold.get_width(); column++) {
@@ -51,6 +30,7 @@ namespace day07 {
                 if (manifold[{row, column}] == '^') {
                     next_row[column - 1] += current_row[column];
                     next_row[column + 1] += current_row[column];
+                    split++;
                 }
                 else {
                     next_row[column] += current_row[column];
@@ -59,7 +39,7 @@ namespace day07 {
             std::swap(current_row, next_row);
             std::fill(next_row.begin(), next_row.end(), 0);
         }
-        
-        return std::reduce(current_row.cbegin(), current_row.cend());
+
+        return part2 ? std::reduce(current_row.cbegin(), current_row.cend()) : split;
     }
 }
--- 2025/day07/day07.h
+++ 2025/day07/day07.h
@@ -7,4 +7,6 @@
 namespace day07 {
     std::int64_t part1(const std::vector<std::string>& rows);
     std::int64_t part2(const std::vector<std::string>& rows);
+
+    int64_t solve(const std::vector<std::string>& rows, bool part2);
 }
