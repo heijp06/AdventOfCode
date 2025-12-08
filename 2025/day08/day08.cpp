f230d22324809cd10e8d1a2fa80a7e490df3a388
Peter van der Heijden <peter.vd.heijden@malvernpanalytical.com>
2025-12-08 08:09:13 +0100

    Checkin WIP.

--- 2025/day08/day08.cpp
+++ 2025/day08/day08.cpp
@@ -1,8 +1,30 @@
+#include <set>
+
 #include "day08.h"
+#include "../../lib/advent.h"
 
 namespace day08 {
     int part1(const std::vector<std::string>& rows) {
-        (void)rows;
+        auto coords = std::vector<coord3d_t>();
+        coords.reserve(rows.size());
+        std::set<item_t> items;
+
+        for (const auto& row : rows) {
+            const auto& fields = advent::ints(row);
+            coords.emplace_back(coord3d{ fields[0] , fields[1], fields[2] });
+        }
+
+        for (size_t i = 0; i < coords.size() - 1; ++i)
+        {
+            const auto& coord1 = coords[i];
+            for (size_t j = i + 1; j < coords.size(); ++j)
+            {
+                const auto& coord2 = coords[j];
+                std::int64_t distance = (coord1.x - coord2.x) * (coord1.x - coord2.x) +
+                const auto& item = item_t{}
+            }
+        }
+
         return -1;
     }
 
--- 2025/day08/day08.h
+++ 2025/day08/day08.h
@@ -1,9 +1,22 @@
 #pragma once
 
+#include <cstdint>
 #include <string>
 #include <vector>
 
 namespace day08 {
+    struct coord3d_t {
+        int x;
+        int y;
+        int z;
+    };
+
+    struct item_t {
+        std::int64_t distance;
+        coord3d p1;
+        coord3d p2;
+    };
+
     int part1(const std::vector<std::string>& rows);
     int part2(const std::vector<std::string>& rows);
 }
