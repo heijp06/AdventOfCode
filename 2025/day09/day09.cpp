4a950ff066730066d4dfd1fef34a7946aa51a13d
Peter van der Heijden <peter.vd.heijden@malvernpanalytical.com>
2025-12-09 07:29:04 +0100

    Solve day 9 part 1.

--- 2025/day09/day09.cpp
+++ 2025/day09/day09.cpp
@@ -1,12 +1,32 @@
+#include <cmath>
+#include <utility>
+
 #include "day09.h"
+#include "../../lib/advent.h"
 
 namespace day09 {
-    int part1(const std::vector<std::string>& rows) {
-        (void)rows;
-        return -1;
+    std::int64_t part1(const std::vector<std::string>& rows) {
+        std::vector<std::pair<std::int64_t, std::int64_t>> pairs;
+        pairs.reserve(rows.size());
+        std::int64_t max_area{};
+
+        for (const auto& row : rows) {
+            const auto& ints = advent::ints<int64_t>(row);
+            pairs.emplace_back(std::make_pair(ints[0], ints[1]));
+        }
+
+        for (const auto& pair1 : pairs) {
+            for (const auto& pair2 : pairs) {
+                auto area =
+                    (std::abs(pair2.first - pair1.first) + 1) * (std::abs(pair2.second - pair1.second) + 1);
+                max_area = std::max(max_area, area);
+            }
+        }
+        
+        return max_area;
     }
 
-    int part2(const std::vector<std::string>& rows) {
+    std::int64_t part2(const std::vector<std::string>& rows) {
         (void)rows;
         return -1;
     }
--- 2025/day09/day09_test.cpp
+++ 2025/day09/day09_test.cpp
@@ -5,11 +5,18 @@
 #include "day09.h"
 
 std::vector<std::string> rows{
-
+    "7,1",
+    "11,1",
+    "11,7",
+    "9,7",
+    "9,5",
+    "2,5",
+    "2,3",
+    "7,3",
 };
 
 TEST_CASE("part1") {
-    REQUIRE(day09::part1(rows) == -1);
+    REQUIRE(day09::part1(rows) == 50);
 }
 
 TEST_CASE("part2") {
