995305d1ba218da82f898c080a96c7f7a18a1efd
Peter van der Heijden <heijp06@gmail.com>
2025-12-10 06:16:37 +0100

    Start day 10.

---
+++ 2025/day10/day10_main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+#include "../../lib/advent.h"
+#include "day10.h"
+
+int main() {
+    const auto& rows = advent::get_rows(2025, 10);
+
+    std::cout << "Part 1: " << day10::part1(rows) << std::endl;
+    std::cout << "Part 2: " << day10::part2(rows) << std::endl;
+
+    return 0;
+}
