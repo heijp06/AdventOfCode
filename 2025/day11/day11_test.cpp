d0cf9c389c49a55a4870db9e674d704160a21455
Peter van der Heijden <peter.vd.heijden@malvernpanalytical.com>
2025-12-11 07:20:13 +0100

    Add test for part 1.

--- 2025/day11/day11_test.cpp
+++ 2025/day11/day11_test.cpp
@@ -5,11 +5,20 @@
 #include "day11.h"
 
 std::vector<std::string> rows{
-
+    "aaa: you hhh",
+    "you: bbb ccc",
+    "bbb: ddd eee",
+    "ccc: ddd eee fff",
+    "ddd: ggg",
+    "eee: out",
+    "fff: out",
+    "ggg: out",
+    "hhh: ccc fff iii",
+    "iii: out",
 };
 
 TEST_CASE("part1") {
-    REQUIRE(day11::part1(rows) == -1);
+    REQUIRE(day11::part1(rows) == 5);
 }
 
 TEST_CASE("part2") {
