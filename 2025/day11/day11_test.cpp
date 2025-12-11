da10fc81db689813fcd5ac0bb3e72d832e615e88
Peter van der Heijden <peter.vd.heijden@malvernpanalytical.com>
2025-12-11 07:17:55 +0100

    Start day 11.

---
+++ 2025/day11/day11_test.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+#include "../../lib/catch.hpp"
+#include "day11.h"
+
+std::vector<std::string> rows{
+
+};
+
+TEST_CASE("part1") {
+    REQUIRE(day11::part1(rows) == -1);
+}
+
+TEST_CASE("part2") {
+    REQUIRE(day11::part2(rows) == -1);
+}
