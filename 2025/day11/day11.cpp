f1426c1ab2c8df77c8464eb8044f436f55884be5
Peter van der Heijden <peter.vd.heijden@malvernpanalytical.com>
2025-12-11 07:33:28 +0100

    WIP

--- 2025/day11/day11.cpp
+++ 2025/day11/day11.cpp
@@ -1,13 +1,34 @@
 #include "day11.h"
+#include "../../lib/advent.h"
 
 namespace day11 {
     int part1(const std::vector<std::string>& rows) {
-        (void)rows;
+        auto& reactor = parse(rows);
+
         return -1;
     }
 
     int part2(const std::vector<std::string>& rows) {
         (void)rows;
         return -1;
+    }
+
+    std::map<std::string, Device> parse(const std::vector<std::string>& rows)
+    {
+        std::map<std::string, Device> result;
+
+        for (const auto& row : rows) {
+            Device device{};
+
+            device.name = row.substr(0, 3);
+            device.outputs = advent::split(row.substr(5), " ");
+            device.paths = device.name == "you" ? 1 : 0;
+            device.pending_inputs = 0;
+
+            result[device.name] = device;
+        }
+
+        // Todo set pending inputs, note that aaa is never reached but inputs to ccc that is reached.
+        return result;
     }
 }
--- 2025/day11/day11.h
+++ 2025/day11/day11.h
@@ -1,9 +1,19 @@
 #pragma once
 
+#include <map>
 #include <string>
 #include <vector>
 
 namespace day11 {
+    struct Device {
+        std::string name;
+        std::vector<std::string> outputs;
+        int paths;
+        int pending_inputs;
+    };
+
     int part1(const std::vector<std::string>& rows);
     int part2(const std::vector<std::string>& rows);
+
+    std::map <std::string, Device> parse(const std::vector<std::string>& rows);
 }
