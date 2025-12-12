89eebb7c306187b958671a91f0368bdd143e9a01
Peter van der Heijden <heijp06@gmail.com>
2025-12-12 06:37:31 +0100

    Start day 12.

---
+++ 2025/day12/day12_test.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+#include "../../lib/catch.hpp"
+#include "day12.h"
+
+std::vector<std::string> rows{
+
+};
+
+TEST_CASE("part1") {
+    REQUIRE(day12::part1(rows) == -1);
+}
+
+TEST_CASE("part2") {
+    REQUIRE(day12::part2(rows) == -1);
+}
