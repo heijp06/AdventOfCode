33d793522637ea23834a961c22a6a42e915d633d
Peter van der Heijden <heijp06@gmail.com>
2025-12-13 19:50:04 +0100

    Add timing.

--- 2025/day10/day10_main.cpp
+++ 2025/day10/day10_main.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 #include <vector>
 
@@ -7,8 +8,14 @@
 int main() {
     const auto& rows = advent::get_rows(2025, 10);
 
+    auto start = std::chrono::steady_clock::now();
+
     std::cout << "Part 1: " << day10::part1(rows) << std::endl;
     std::cout << "Part 2: " << day10::part2(rows) << std::endl;
+
+    auto end = std::chrono::steady_clock::now();
+    const std::chrono::duration<double> elapsed_seconds{end - start};
+    std::cout << elapsed_seconds.count() << std::endl;
 
     return 0;
 }
