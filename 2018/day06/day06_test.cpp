6b96de48bb51cd5475b11ae66c2e285870298252
Peter van der Heijden <heijp06@gmail.com>
2025-12-29 14:43:45 +0100

    Start day 6.

---
+++ 2018/day06/day06_test.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+#include "../../lib/catch.hpp"
+#include "day06.h"
+
+std::vector<std::string> rows{
+
+};
+
+TEST_CASE("part1") {
+    REQUIRE(day06::part1(rows) == -1);
+}
+
+TEST_CASE("part2") {
+    REQUIRE(day06::part2(rows) == -1);
+}
