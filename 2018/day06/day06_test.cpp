082d441392c20c04497a8dce92eb917d62d506be
Peter van der Heijden <heijp06@gmail.com>
2025-12-29 14:46:03 +0100

    Implement test.

--- 2018/day06/day06_test.cpp
+++ 2018/day06/day06_test.cpp
@@ -4,12 +4,10 @@
 #include "../../lib/catch.hpp"
 #include "day06.h"
 
-std::vector<std::string> rows{
-
-};
+std::vector<std::string> rows{"1, 1", "1, 6", "8, 3", "3, 4", "5, 5", "8, 9"};
 
 TEST_CASE("part1") {
-    REQUIRE(day06::part1(rows) == -1);
+    REQUIRE(day06::part1(rows) == 17);
 }
 
 TEST_CASE("part2") {
