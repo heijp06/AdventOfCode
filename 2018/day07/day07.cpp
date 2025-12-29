d6e1750e1402155a55ec56f1d6a0d716f2a99872
Peter van der Heijden <heijp06@gmail.com>
2025-12-29 20:21:44 +0100

    Start day 7 2018.

---
+++ 2018/day07/day07.cpp
@@ -0,0 +1,13 @@
+#include "day07.h"
+
+namespace day07 {
+    int part1(const std::vector<std::string>& rows) {
+        (void)rows;
+        return -1;
+    }
+
+    int part2(const std::vector<std::string>& rows) {
+        (void)rows;
+        return -1;
+    }
+}
---
+++ 2018/day07/day07_test.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+#include "../../lib/catch.hpp"
+#include "day07.h"
+
+std::vector<std::string> rows{
+
+};
+
+TEST_CASE("part1") {
+    REQUIRE(day07::part1(rows) == -1);
+}
+
+TEST_CASE("part2") {
+    REQUIRE(day07::part2(rows) == -1);
+}
