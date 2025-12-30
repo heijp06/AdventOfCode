1547c2fffa7489a6f70c39a2d1215dc572ee03cc
Peter van der Heijden <heijp06@gmail.com>
2025-12-30 20:15:54 +0100

    Create reader.

--- 2018/day08/day08.cpp
+++ 2018/day08/day08.cpp
@@ -3,7 +3,7 @@
 
 namespace day08 {
     int part1(const std::vector<std::string>& rows) {
-        const auto& numbers = advent::ints(rows[0]);
+        Reader reader{advent::ints(rows[0])};
 
         return -1;
     }
@@ -11,5 +11,11 @@ namespace day08 {
     int part2(const std::vector<std::string>& rows) {
         (void)rows;
         return -1;
+    }
+
+    Reader::Reader(const std::vector<int>& numbers) : numbers_{numbers}, pos_{0} {}
+
+    int day08::Reader::read() {
+        return numbers_[pos_++];
     }
 }
--- 2018/day08/day08.h
+++ 2018/day08/day08.h
@@ -4,6 +4,16 @@
 #include <vector>
 
 namespace day08 {
+    class Reader {
+    public:
+        Reader(const std::vector<int>& numbers);
+        int read();
+
+    private:
+        const std::vector<int>& numbers_;
+        int pos_;
+    };
+
     int part1(const std::vector<std::string>& rows);
     int part2(const std::vector<std::string>& rows);
 }
