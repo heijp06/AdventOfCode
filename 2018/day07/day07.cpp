404ea1db6a9e626f5afa8afab57bc1722e4f76c8
Peter van der Heijden <heijp06@gmail.com>
2025-12-30 16:50:55 +0100

    Day 7 part 1 example succeeds.

--- 2018/day07/day07.cpp
+++ 2018/day07/day07.cpp
@@ -1,9 +1,61 @@
+#include <algorithm>
+#include <iterator>
+#include <set>
+
 #include "day07.h"
 
 namespace day07 {
     std::string part1(const std::vector<std::string>& rows) {
-        (void)rows;
-        return "";
+        auto& pairs = parse(rows);
+        std::string result{};
+        std::set<char> available{};
+        std::set<char> to{};
+
+        while (!pairs.empty()) {
+            std::set<char> from{};
+            to.clear();
+
+            for (const auto& pair : pairs) {
+                from.insert(pair.first);
+                to.insert(pair.second);
+            }
+
+            std::vector<char> difference{};
+            difference.reserve(26);
+            std::set_difference(from.begin(), from.end(), to.begin(), to.end(), std::back_inserter(difference));
+
+            for (const auto& c : difference) {
+                pairs.erase(std::remove_if(pairs.begin(), pairs.end(), [&](auto& p) { return p.first == c; }), pairs.cend());
+            }
+
+            available.insert(difference.cbegin(), difference.cend());
+
+            const auto c = *(available.cbegin());
+            available.erase(available.cbegin());
+
+            result += c;
+        }
+
+        for (const auto& c : available) {
+            result += c;
+        }
+
+        for (const auto& c : to) {
+            result += c;
+        }
+
+        return result;
+    }
+
+    std::vector<std::pair<char, char>> parse(const std::vector<std::string>& rows) {
+        std::vector<std::pair<char, char>> pairs{};
+        pairs.reserve(rows.size());
+
+        for (const auto& row : rows) {
+            pairs.push_back({row[5], row[36]});
+        }
+
+        return pairs;
     }
 
     int part2(const std::vector<std::string>& rows) {
--- 2018/day07/day07.h
+++ 2018/day07/day07.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <string>
+#include <utility>
 #include <vector>
 
 namespace day07 {
     std::string part1(const std::vector<std::string>& rows);
     int part2(const std::vector<std::string>& rows);
+
+    std::vector<std::pair<char, char>> parse(const std::vector<std::string>& rows);
 }
