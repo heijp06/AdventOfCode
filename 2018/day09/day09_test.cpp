35c67b33afea0346fd2e51c3147b434949d1d461
Peter van der Heijden <heijp06@gmail.com>
2025-12-31 16:23:56 +0100

    Create tests for part 1.

--- 2018/day09/day09_test.cpp
+++ 2018/day09/day09_test.cpp
@@ -9,7 +9,21 @@ std::vector<std::string> rows{
 };
 
 TEST_CASE("part1") {
-    REQUIRE(day09::part1(rows) == -1);
+    struct testdata {
+        std::vector<std::string> game;
+        int score;
+    };
+
+    const auto& item = GENERATE(
+        testdata{{"9 players; last marble is worth 25 points"}, 32},
+        testdata{{"10 players; last marble is worth 1618 points"}, 8317},
+        testdata{{"13 players; last marble is worth 7999 points"}, 146373},
+        testdata{{"17 players; last marble is worth 1104 points"}, 2764},
+        testdata{{"21 players; last marble is worth 6111 points"}, 54718},
+        testdata{{"30 players; last marble is worth 5807 points"}, 37305}
+    );
+
+    REQUIRE(day09::part1(item.game) == item.score);
 }
 
 TEST_CASE("part2") {
