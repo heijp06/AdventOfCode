666695f68bf5ee4e723cca7584b9d0f8708cd175
Peter van der Heijden <heijp06@gmail.com>
2025-12-31 16:33:09 +0100

    Create Node.

--- 2018/day09/day09.h
+++ 2018/day09/day09.h
@@ -1,9 +1,16 @@
 #pragma once
 
+#include <memory>
 #include <string>
 #include <vector>
 
 namespace day09 {
+    struct Node {
+        int value;
+        std::shared_ptr<Node> previous;
+        std::shared_ptr<Node> next;
+    };
+
     int part1(const std::vector<std::string>& rows);
     int part2(const std::vector<std::string>& rows);
 }
