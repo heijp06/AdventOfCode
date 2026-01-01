f5f1e20f35338ed40e671d1be3f6c12598775abe
Peter van der Heijden <heijp06@gmail.com>
2026-01-01 20:51:04 +0100

    Use a grid that has cumulative totals from top left to bottom right.

--- 2018/day11/day11.cpp
+++ 2018/day11/day11.cpp
@@ -1,5 +1,4 @@
 #include <algorithm>
-#include <iostream>
 #include <string>
 
 #include "day11.h"
@@ -17,14 +16,11 @@ namespace day11 {
 
         std::pair<std::string, int> max{};
         for (int block_size = 1; block_size <= size; block_size++) {
-            std::cout << block_size << " ";
             const auto& solution = solve(grid, block_size);
             if (max.second < solution.second) {
                 max = {solution.first + "," + std::to_string(block_size), solution.second};
             }
         }
-
-        std::cout << std::endl;
 
         return max.first;
     }
@@ -36,12 +32,10 @@ namespace day11 {
 
         for (int x = 1; x <= size - (block_size - 1); x++) {
             for (int y = 1; y <= size - (block_size - 1); y++) {
-                int total{};
-                for (int delta_x = 0; delta_x < block_size; delta_x++) {
-                    for (int delta_y = 0; delta_y < block_size; delta_y++) {
-                        total += grid[x + delta_x][y + delta_y];
-                    }
-                }
+                int total =
+                    grid[x + block_size - 1][y + block_size - 1]
+                    - grid[x - 1][y + block_size - 1] - grid[x + block_size - 1][y - 1]
+                    + grid[x - 1][y - 1];
                 if (total > max) {
                     max = total;
                     x_max = x;
@@ -54,19 +48,21 @@ namespace day11 {
     }
 
     std::vector<std::vector<int>> create_grid(const std::vector<std::string>& rows) {
+        // A cell (x, y) in the grid contains the sum of all power levels in the rectangle from 1 to x and 1 to y.
+        // This is somewhat complex but allows calculating sums of blocks fast.
         const auto serial_number = advent::ints(rows.front()).front();
         std::vector<std::vector<int>> grid{size + 1, std::vector(size + 1, 0)};
 
-        for (int x = 1; x <= size; x++) {
-            int rack_id = x + 10;
-            for (int y = 1; y <= size; y++) {
+        for (int y = 1; y <= size; y++) {
+            for (int x = 1; x <= size; x++) {
+                int rack_id = x + 10;
                 int power_level = rack_id * y;
                 power_level += serial_number;
                 power_level *= rack_id;
                 power_level %= 1000;
                 power_level /= 100;
                 power_level -= 5;
-                grid[x][y] = power_level;
+                grid[x][y] = grid[x - 1][y] + grid[x][y - 1] - grid[x - 1][y - 1] + power_level;
             }
         }
 
