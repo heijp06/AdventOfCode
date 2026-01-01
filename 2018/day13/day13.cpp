0c24439191d7cb55e734b36a572310678836c81b
Peter van der Heijden <heijp06@gmail.com>
2026-01-01 23:15:03 +0100

    Start day 13.

---
+++ 2018/day13/day13.cpp
@@ -0,0 +1,13 @@
+#include "day13.h"
+
+namespace day13 {
+    int part1(const std::vector<std::string>& rows) {
+        (void)rows;
+        return -1;
+    }
+
+    int part2(const std::vector<std::string>& rows) {
+        (void)rows;
+        return -1;
+    }
+}
---
+++ 2018/day13/day13_test.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+#include "../../lib/catch.hpp"
+#include "day13.h"
+
+std::vector<std::string> rows{
+
+};
+
+TEST_CASE("part1") {
+    REQUIRE(day13::part1(rows) == -1);
+}
+
+TEST_CASE("part2") {
+    REQUIRE(day13::part2(rows) == -1);
+}
