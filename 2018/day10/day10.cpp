b5383e08abec870d882daacbf151922046cd609c
Peter van der Heijden <heijp06@gmail.com>
2026-01-01 12:01:52 +0100

    Write code to solve part 1, needs improvement.

--- 2018/day10/day10.cpp
+++ 2018/day10/day10.cpp
@@ -1,8 +1,53 @@
+#include <algorithm>
+#include <iostream>
+
 #include "day10.h"
 
 namespace day10 {
     int part1(const std::vector<std::string>& rows) {
-        (void)rows;
+        const int expected_height = 10;
+
+        auto lights = std::vector<Light>{};
+        lights.reserve(rows.size());
+
+        for (const auto& row : rows) {
+            const auto& fields = advent::ints(row);
+            lights.push_back({{fields[1], fields[0]}, {fields[3], fields[2]}});
+        }
+
+        int actual_height{};
+        int min_row{};
+        int max_row{};
+        do {
+            for (auto& light : lights) {
+                light.position = light.position + light.velocity;
+            }
+
+            min_row = std::min_element(lights.cbegin(), lights.cend(), [](const auto& left, const auto& right) {
+                return left.position.row < right.position.row;
+                })->position.row;
+            max_row = std::max_element(lights.cbegin(), lights.cend(), [](const auto& left, const auto& right) {
+                return left.position.row < right.position.row;
+                })->position.row;
+            actual_height = max_row - min_row + 1;
+
+        } while (actual_height > expected_height);
+
+        const auto& min_column = std::min_element(lights.cbegin(), lights.cend(), [](const auto& left, const auto& right) {
+            return left.position.column < right.position.column;
+            })->position.column;
+        const auto& max_column = std::max_element(lights.cbegin(), lights.cend(), [](const auto& left, const auto& right) {
+            return left.position.column < right.position.column;
+            })->position.column;
+
+        auto grid = advent::grid(max_row - min_row + 1, max_column - min_column + 1);
+
+        for (auto& light : lights) {
+            grid[{light.position.row - min_row, light.position.column - min_column}] = '#';
+        }
+        std::cout << std::endl;
+        grid.draw();
+
         return -1;
     }
 
--- 2018/day10/day10.h
+++ 2018/day10/day10.h
@@ -3,7 +3,14 @@
 #include <string>
 #include <vector>
 
+#include "../../lib/advent.h"
+
 namespace day10 {
+    struct Light {
+        advent::coord position;
+        advent::direction velocity;
+    };
+
     int part1(const std::vector<std::string>& rows);
     int part2(const std::vector<std::string>& rows);
 }
