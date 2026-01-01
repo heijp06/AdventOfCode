90e08cb785a5027e9e00b72773705eb3e9dd7c8c
Peter van der Heijden <heijp06@gmail.com>
2026-01-01 10:55:17 +0100

    Solve day 9 part 1.

--- 2018/day09/day09.cpp
+++ 2018/day09/day09.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+
 #include "day09.h"
 #include "../../lib/advent.h"
 
@@ -17,9 +19,37 @@ namespace day09 {
 
         int current_player{};
         for (int marble = 1; marble <= last; marble++) {
+            if (marble % 23) {
+                auto to_add = std::make_shared<Node>();
+                auto& where = current->next;
+
+                to_add->value = marble;
+                to_add->next = where->next;
+                to_add->previous = where;
+
+                where->next->previous = to_add;
+                where->next = to_add;
+
+                current = to_add;
+            }
+            else {
+                auto& to_remove = current;
+                for (size_t i = 0; i < 7; i++) {
+                    to_remove = to_remove->previous;
+                }
+
+                scores[current_player] += marble + to_remove->value;
+
+                to_remove->previous->next = to_remove->next;
+                to_remove->next->previous = to_remove->previous;
+
+                current = to_remove->next;
+            }
+
+            current_player = (current_player + 1) % players;
         }
 
-        return current->next->value;
+        return *std::max_element(scores.cbegin(), scores.cend());
     }
 
     int part2(const std::vector<std::string>& rows) {
