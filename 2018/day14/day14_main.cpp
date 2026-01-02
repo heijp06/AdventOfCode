55e6e132b8ce35a042be568813e89049b673d115
Peter van der Heijden <heijp06@gmail.com>
2026-01-02 20:44:26 +0100

    Start day 14.

---
+++ 2018/day14/day14_main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+#include "../../lib/advent.h"
+#include "day14.h"
+
+int main() {
+    const auto& rows = advent::get_rows(2018, 14);
+
+    std::cout << "Part 1: " << day14::part1(rows) << std::endl;
+    std::cout << "Part 2: " << day14::part2(rows) << std::endl;
+
+    return 0;
+}
---
+++ 2018/day14/day14_test.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+#include "../../lib/catch.hpp"
+#include "day14.h"
+
+std::vector<std::string> rows{
+
+};
+
+TEST_CASE("part1") {
+    REQUIRE(day14::part1(rows) == -1);
+}
+
+TEST_CASE("part2") {
+    REQUIRE(day14::part2(rows) == -1);
+}
