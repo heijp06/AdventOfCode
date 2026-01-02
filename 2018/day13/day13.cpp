30c16e5a81e69ab513e9f42b277c3ef7ec090d5e
Peter van der Heijden <heijp06@gmail.com>
2026-01-02 16:42:52 +0100

    Parse grid and carts.

--- 2018/day13/day13.cpp
+++ 2018/day13/day13.cpp
@@ -1,13 +1,39 @@
 #include "day13.h"
 
 namespace day13 {
+    static bool operator<(const Cart& left, const Cart& right) {
+        if (left.position == right.position) return left.direction < right.direction;
+        return left.position < right.position;
+    }
+
     std::string part1(const std::vector<std::string>& rows) {
-        (void)rows;
+        advent::grid grid{rows};
+        auto current = parse_carts(grid);
+
         return "";
     }
 
     int part2(const std::vector<std::string>& rows) {
         (void)rows;
         return -1;
+    }
+
+    std::set<Cart> parse_carts(advent::grid& grid) {
+        std::set<Cart> carts{};
+        std::vector<std::pair<std::string, advent::direction>> directions{
+            {">-", advent::direction::right()},
+            {"v|", advent::direction::down()},
+            {"<-", advent::direction::left()},
+            {"^|", advent::direction::up()}
+        };
+
+        for (const auto& direction : directions) {
+            for (const auto& position : grid.find_all(direction.first[0])) {
+                carts.insert({position, direction.second});
+                grid[position] = direction.first[1];
+            }
+        }
+
+        return carts;
     }
 }
--- 2018/day13/day13.h
+++ 2018/day13/day13.h
@@ -1,9 +1,19 @@
 #pragma once
 
+#include <set>
 #include <string>
 #include <vector>
 
+#include "../../lib/advent.h"
+
 namespace day13 {
+    struct Cart {
+        advent::coord position;
+        advent::direction direction;
+    };
+
     std::string part1(const std::vector<std::string>& rows);
     int part2(const std::vector<std::string>& rows);
+
+    std::set<Cart> parse_carts(advent::grid& grid);
 }
