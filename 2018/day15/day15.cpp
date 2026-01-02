fc9f6494361206d2baed954775089a4fc1eaaf95
Peter van der Heijden <heijp06@gmail.com>
2026-01-02 23:11:02 +0100

    Start day 15.

---
+++ 2018/day15/day15.cpp
@@ -0,0 +1,13 @@
+#include "day15.h"
+
+namespace day15 {
+    int part1(const std::vector<std::string>& rows) {
+        (void)rows;
+        return -1;
+    }
+
+    int part2(const std::vector<std::string>& rows) {
+        (void)rows;
+        return -1;
+    }
+}
---
+++ 2018/day15/day15_main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+#include "../../lib/advent.h"
+#include "day15.h"
+
+int main() {
+    const auto& rows = advent::get_rows(2018, 15);
+
+    std::cout << "Part 1: " << day15::part1(rows) << std::endl;
+    std::cout << "Part 2: " << day15::part2(rows) << std::endl;
+
+    return 0;
+}
