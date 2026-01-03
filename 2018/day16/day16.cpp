0f3591ec4a93bb91d111e05082ac19a19dd22f63
Peter van der Heijden <peter.vd.heijden@malvernpanalytical.com>
2026-01-03 21:55:25 +0100

    Make test for day 16 part 1 pass.

--- 2018/day16/day16.cpp
+++ 2018/day16/day16.cpp
@@ -13,6 +13,20 @@ namespace day16 {
             const auto& before = advent::ints(*it++);
             const auto& instruction = advent::ints(*it++);
             const auto& after = advent::ints(*it++);
+            int counter{};
+
+            for (const auto& pair : Device::functions) {
+                Device device{};
+                device.registers = before;
+                pair.second(device, instruction[1], instruction[2], instruction[3]);
+                if (device.registers == after) {
+                    counter++;
+                }
+            }
+
+            if (counter >= 3) {
+                result++;
+            }
         }
 
         return result;
@@ -22,4 +36,96 @@ namespace day16 {
         (void)rows;
         return -1;
     }
+
+    Device::Device() : registers(4, 0) {
+    }
+
+    void Device::execute(const std::string& opcode, int a, int b, int c) {
+        functions[opcode](*this, a, b, c);
+    }
+
+    void Device::addr(int a, int b, int c) {
+        registers[c] = registers[a] + registers[b];
+    }
+
+    void Device::addi(int a, int b, int c) {
+        registers[c] = registers[a] + b;
+    }
+
+    void Device::mulr(int a, int b, int c) {
+        registers[c] = registers[a] * registers[b];
+    }
+
+    void Device::muli(int a, int b, int c) {
+        registers[c] = registers[a] * b;
+    }
+
+    void Device::banr(int a, int b, int c) {
+        registers[c] = registers[a] & registers[b];
+    }
+
+    void Device::bani(int a, int b, int c) {
+        registers[c] = registers[a] & b;
+    }
+
+    void Device::borr(int a, int b, int c) {
+        registers[c] = registers[a] | registers[b];
+    }
+
+    void Device::bori(int a, int b, int c) {
+        registers[c] = registers[a] | b;
+    }
+
+    void Device::setr(int a, int b, int c) {
+        (void)b;
+        registers[c] = registers[a];
+    }
+
+    void Device::seti(int a, int b, int c) {
+        (void)b;
+        registers[c] = a;
+    }
+
+    void Device::gtir(int a, int b, int c) {
+        registers[c] = a > registers[b] ? 1 : 0;
+    }
+
+    void Device::gtri(int a, int b, int c) {
+        registers[c] = registers[a] > b ? 1 : 0;
+    }
+
+    void Device::gtrr(int a, int b, int c) {
+        registers[c] = registers[a] > registers[b] ? 1 : 0;
+    }
+
+    void Device::eqir(int a, int b, int c) {
+        registers[c] = a == registers[b] ? 1 : 0;
+    }
+
+    void Device::eqri(int a, int b, int c) {
+        registers[c] = registers[a] == b ? 1 : 0;
+    }
+
+    void Device::eqrr(int a, int b, int c) {
+        registers[c] = registers[a] == registers[b] ? 1 : 0;
+    }
+
+    std::unordered_map<std::string, std::function<void(Device&, int, int, int)>> Device::functions = {
+        {"addr", &Device::addr},
+        {"addi", &Device::addi},
+        {"mulr", &Device::mulr},
+        {"muli", &Device::muli},
+        {"banr", &Device::banr},
+        {"bani", &Device::bani},
+        {"borr", &Device::borr},
+        {"bori", &Device::bori},
+        {"setr", &Device::setr},
+        {"seti", &Device::seti},
+        {"gtir", &Device::gtir},
+        {"gtri", &Device::gtri},
+        {"gtrr", &Device::gtrr},
+        {"eqir", &Device::eqir},
+        {"eqri", &Device::eqri},
+        {"eqrr", &Device::eqrr}
+    };
 }
--- 2018/day16/day16.h
+++ 2018/day16/day16.h
@@ -1,9 +1,35 @@
 #pragma once
 
+#include <functional>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 namespace day16 {
+    struct Device {
+        Device();
+        void execute(const std::string& opcode, int a, int b, int c);
+        void addr(int a, int b, int c);
+        void addi(int a, int b, int c);
+        void mulr(int a, int b, int c);
+        void muli(int a, int b, int c);
+        void banr(int a, int b, int c);
+        void bani(int a, int b, int c);
+        void borr(int a, int b, int c);
+        void bori(int a, int b, int c);
+        void setr(int a, int b, int c);
+        void seti(int a, int b, int c);
+        void gtir(int a, int b, int c);
+        void gtri(int a, int b, int c);
+        void gtrr(int a, int b, int c);
+        void eqir(int a, int b, int c);
+        void eqri(int a, int b, int c);
+        void eqrr(int a, int b, int c);
+        std::vector<int> registers;
+
+        static std::unordered_map<std::string, std::function<void(Device&, int, int, int)>> functions;
+    };
+
     int part1(const std::vector<std::string>& rows);
     int part2(const std::vector<std::string>& rows);
 }
