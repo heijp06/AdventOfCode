2ca1201b977d81092e3e848638af3862ff2eafe4
Peter van der Heijden <peter.vd.heijden@malvernpanalytical.com>
2026-01-03 20:24:54 +0100

    Create test for part 1.

--- 2018/day16/day16_test.cpp
+++ 2018/day16/day16_test.cpp
@@ -5,11 +5,14 @@
 #include "day16.h"
 
 std::vector<std::string> rows{
-
+    "Before: [3, 2, 1, 1]",
+    "9 2 1 2",
+    "After:  [3, 2, 2, 1]",
+    ""
 };
 
 TEST_CASE("part1") {
-    REQUIRE(day16::part1(rows) == -1);
+    REQUIRE(day16::part1(rows) == 1);
 }
 
 TEST_CASE("part2") {
