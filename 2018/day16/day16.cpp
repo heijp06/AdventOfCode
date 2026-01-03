703603e6fe9b1ddc9be972ab541b7d3d7014ac44
Peter van der Heijden <peter.vd.heijden@malvernpanalytical.com>
2026-01-03 20:38:07 +0100

    Implement parsing.

--- 2018/day16/day16.cpp
+++ 2018/day16/day16.cpp
@@ -1,9 +1,21 @@
 #include "day16.h"
+#include "../../lib/advent.h"
 
 namespace day16 {
     int part1(const std::vector<std::string>& rows) {
-        (void)rows;
-        return -1;
+        int result{};
+
+        for (auto it = rows.cbegin(); it != rows.cend(); ++it) {
+            if (it->empty()) {
+                break;
+            }
+
+            const auto& before = advent::ints(*it++);
+            const auto& instruction = advent::ints(*it++);
+            const auto& after = advent::ints(*it++);
+        }
+
+        return result;
     }
 
     int part2(const std::vector<std::string>& rows) {
