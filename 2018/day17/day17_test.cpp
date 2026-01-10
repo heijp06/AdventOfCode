ff481f1add371a62d82c8974834229b34591d2f4
Peter van der Heijden <heijp06@gmail.com>
2026-01-10 19:39:25 +0100

    Add test for day 17 part 1.

--- 2018/day17/day17_test.cpp
+++ 2018/day17/day17_test.cpp
@@ -5,11 +5,18 @@
 #include "day17.h"
 
 std::vector<std::string> rows{
-
+    "x=495, y=2..7",
+    "y=7, x=495..501",
+    "x=501, y=3..7",
+    "x=498, y=2..4",
+    "x=506, y=1..2",
+    "x=498, y=10..13",
+    "x=504, y=10..13",
+    "y=13, x=498..504"
 };
 
 TEST_CASE("part1") {
-    REQUIRE(day17::part1(rows) == -1);
+    REQUIRE(day17::part1(rows) == 57);
 }
 
 TEST_CASE("part2") {
