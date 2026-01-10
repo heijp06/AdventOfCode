0b57e445e11c2c227e65dc4c522d72d0f0d4d0f0
Peter van der Heijden <heijp06@gmail.com>
2026-01-10 15:53:11 +0100

    Create tests for part 2.

--- 2018/day15/day15_test.cpp
+++ 2018/day15/day15_test.cpp
@@ -4,8 +4,57 @@
 #include "../../lib/catch.hpp"
 #include "day15.h"
 
-std::vector<std::string> rows{
-
+const std::vector<std::vector<std::string>> grids = {
+    {
+        "#######",
+        "#.G...#",
+        "#...EG#",
+        "#.#.#G#",
+        "#..G#E#",
+        "#.....#",
+        "#######"},
+    {
+        "#######",
+        "#G..#E#",
+        "#E#E.E#",
+        "#G.##.#",
+        "#...#E#",
+        "#...E.#",
+        "#######"},
+    {
+        "#######",
+        "#E..EG#",
+        "#.#G.E#",
+        "#E.##E#",
+        "#G..#.#",
+        "#..E#.#",
+        "#######"},
+    {
+        "#######",
+        "#E.G#.#",
+        "#.#G..#",
+        "#G.#.G#",
+        "#G..#.#",
+        "#...E.#",
+        "#######"},
+    {
+        "#######",
+        "#.E...#",
+        "#.#..G#",
+        "#.###.#",
+        "#E#G#G#",
+        "#...#G#",
+        "#######"},
+    {
+        "#########",
+        "#G......#",
+        "#.E.#...#",
+        "#..##..G#",
+        "#...##..#",
+        "#...#...#",
+        "#.G...G.#",
+        "#.....G.#",
+        "#########"}
 };
 
 TEST_CASE("part1") {
@@ -15,61 +64,30 @@ TEST_CASE("part1") {
     };
 
     const auto& item = GENERATE(
-        testdata{{
-            "#######",
-            "#.G...#",
-            "#...EG#",
-            "#.#.#G#",
-            "#..G#E#",
-            "#.....#",
-            "#######"}, 27730},
-        testdata{{
-            "#######",
-            "#G..#E#",
-            "#E#E.E#",
-            "#G.##.#",
-            "#...#E#",
-            "#...E.#",
-            "#######"}, 36334},
-        testdata{{
-            "#######",
-            "#E..EG#",
-            "#.#G.E#",
-            "#E.##E#",
-            "#G..#.#",
-            "#..E#.#",
-            "#######"}, 39514},
-        testdata{{
-            "#######",
-            "#E.G#.#",
-            "#.#G..#",
-            "#G.#.G#",
-            "#G..#.#",
-            "#...E.#",
-            "#######"}, 27755},
-        testdata{{
-            "#######",
-            "#.E...#",
-            "#.#..G#",
-            "#.###.#",
-            "#E#G#G#",
-            "#...#G#",
-            "#######"}, 28944},
-        testdata{{
-            "#########",
-            "#G......#",
-            "#.E.#...#",
-            "#..##..G#",
-            "#...##..#",
-            "#...#...#",
-            "#.G...G.#",
-            "#.....G.#",
-            "#########"}, 18740}
-        );
+        testdata{grids[0], 27730},
+        testdata{grids[1], 36334},
+        testdata{grids[2], 39514},
+        testdata{grids[3], 27755},
+        testdata{grids[4], 28944},
+        testdata{grids[5], 18740}
+    );
 
     REQUIRE(day15::part1(item.rows) == item.outcome);
 }
 
 TEST_CASE("part2") {
-    REQUIRE(day15::part2(rows) == -1);
+    struct testdata {
+        std::vector<std::string> rows;
+        int outcome;
+    };
+
+    const auto& item = GENERATE(
+        testdata{grids[0], 4988},
+        testdata{grids[2], 31284},
+        testdata{grids[3], 3478},
+        testdata{grids[4], 6474},
+        testdata{grids[5], 1140}
+    );
+
+    REQUIRE(day15::part2(item.rows) == item.outcome);
 }
